b67977fea45baf8c0ea1fc9bf044375694a565ab
Miguel Garzon <asopapos@gmail.com>
2011-08-16 21:14:49 +0000

    Updating code to xtext 2.0
    git-svn-id: http://umple.googlecode.com/svn/trunk@629 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src/cruise/umple/UmpleRuntimeModule.java
+++ cruise.umple/src/cruise/umple/UmpleRuntimeModule.java
@@ -3,11 +3,9 @@
  */
 package cruise.umple;
 
-
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class UmpleRuntimeModule extends cruise.umple.AbstractUmpleRuntimeModule {
 
-	
 }
--- cruise.umple/src/cruise/umple/validation/UmpleJavaValidator.java
+++ cruise.umple/src/cruise/umple/validation/UmpleJavaValidator.java
@@ -6,7 +6,7 @@ public class UmpleJavaValidator extends AbstractUmpleJavaValidator {
 //	@Check
 //	public void checkGreetingStartsWithCapital(Greeting greeting) {
 //		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
+//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
 //		}
 //	}
 
