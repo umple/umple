9b123e43d8f39f20a89b84dece359e3a3f9af203
Miguel Garzon <asopapos@gmail.com>
2011-08-17 14:10:41 +0000

    Generated code plus libraries
    git-svn-id: http://umple.googlecode.com/svn/trunk@635 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src/cruise/umple/sync/EditAssociationAction.java
+++ cruise.umple/src/cruise/umple/sync/EditAssociationAction.java
@@ -1,5 +1,5 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.12.1.605 modeling language!*/
+/*This code was generated using the UMPLE 1.13.0.605 modeling language!*/
 
 package cruise.umple.sync;
 import cruise.umple.compiler.*;
