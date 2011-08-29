39122997485b425df3260434bde6c6f9f0ddc0c9
Andrew Forward <aforward@gmail.com>
2011-08-29 13:41:43 +0000

    Upgraded umpleonline deployment to use rsync - much safer and easier than the 'replace'
    git-svn-id: http://umple.googlecode.com/svn/trunk@660 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- build/deploy/deployUmpleonline.sh
+++ build/deploy/deployUmpleonline.sh
@@ -1,61 +1,15 @@
 #!/bin/bash
-echo "Moving Umple Reference Documentation"
-rm -rf /h/ralph/sites/www/html/umple
+echo "Generating Updated Umple Reference Documentation"
 cd /h/ralph/umple/trunk/ && java -jar dist/umpledocs.jar build/reference dist/cruise.umple/reference
-cp -r /h/ralph/umple/trunk/dist/cruise.umple/reference /h/ralph/sites/www/html/
-mv /h/ralph/sites/www/html/reference/ /h/ralph/sites/www/html/umple
 
-echo "Removing UmpleOnline"
-rm -rf /h/ralph/sites/www/html/umpleonline
-rm -rf /h/ralph/sites/www/html/umpleonlineold
+echo "Synchronizing Umple Reference Documentation"
+rsync -vaz --exclude=.svn/ --exclude=reference/ /h/ralph/sites/www/html/umple /h/ralph/sites/www/html
+rsync -var --exclude=.svn/ /h/ralph/sites/www/html/reference /h/ralph/sites/www/html/umple
 
-echo "Removing BumpleOnline"
-rm -rf /h/ralph/sites/www/html/bumpleonline
+echo "Synchronizing UmpleOnline"
+rsync -vaz --exclude=.svn/ --exclude=ump/ /h/ralph/umple/trunk/umpleonline /h/ralph/sites/www/html
 
-echo "Moving UmpleOnline"
-cp -r /h/ralph/umple/trunk/umpleonline /h/ralph/sites/www/html/
-
-echo "Moving UmpleOnlineOld"
-cp -r /h/ralph/umple/trunk/dist/playground /h/ralph/sites/www/html/
-mv /h/ralph/sites/www/html/playground /h/ralph/sites/www/html/umpleonlineold
-
-echo "Moving BumpleOnline"
-cp -r /h/ralph/cruise/dev/bumpleonline /h/ralph/sites/www/html/
-
-echo "Updating Permissions"
-cd /h/ralph/sites/www/html/
-chown -R ralph umple
-chgrp -R ralph umple
-chown -R ralph umpleonline
-chgrp -R ralph umpleonline
-chown -R ralph umpleonlineold
-chgrp -R ralph umpleonlineold
-chown -R ralph bumpleonline
-chgrp -R ralph bumpleonline
-
-cd /h/ralph/sites/www/html/umpleonline
-chown -R apache ump
-chgrp -R apache ump
-
-cd /h/ralph/sites/www/html/umpleonlineold
-chown -R apache ump
-chgrp -R apache ump
-
-cd /h/ralph/sites/www/html/bumpleonline
-chown -R apache bump
-chgrp -R apache bump
-
-echo "Removing .svn files"
-cd /h/ralph/sites/www/html/umple
-rm -rf `find . -type d -name .svn`
-
-cd /h/ralph/sites/www/html/umpleonline
-rm -rf `find . -type d -name .svn`
-
-cd /h/ralph/sites/www/html/umpleonlineold
-rm -rf `find . -type d -name .svn`
-
-cd /h/ralph/sites/www/html/bumpleonline
-rm -rf `find . -type d -name .svn`
+echo "Synchronizing BumpleOnline"
+rsync -vaz --exclude=.svn/ --exclude=bump/ /h/ralph/sites/www/html/bumpleonline /h/ralph/sites/www/html
 
 echo "done."
