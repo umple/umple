04875f7516e397f042b4502aaea93ef5a64d50a1
Andrew Forward <aforward@gmail.com>
2011-10-24 01:56:59 +0000

    Upgraded umple itself to 1.14
    git-svn-id: http://umple.googlecode.com/svn/trunk@960 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src/cruise/vml/CodeSnippet.java
+++ cruise.umple/src/cruise/vml/CodeSnippet.java
@@ -1,5 +1,5 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.13.0.957 modeling language!*/
+/*This code was generated using the UMPLE 1.14.0.957 modeling language!*/
 
 package cruise.vml;
 
