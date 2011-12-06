495d85a872c093fb454fcd14c9d31ad0071e8943
Andrew Forward <aforward@gmail.com>
2011-12-06 01:49:35 +0000

    Added full sm trace model
    git-svn-id: http://umple.googlecode.com/svn/trunk@1080 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ examples/smtrace/trace.cmd
@@ -0,0 +1,73 @@
+new example.StateMachineTest
+e2
+show getStatusFullName
+e5
+show getStatusFullName
+e2
+show getStatusFullName
+e5
+show getStatusFullName
+e5
+show getStatusFullName
+e1
+show getStatusFullName
+e3
+show getStatusFullName
+e3
+show getStatusFullName
+e5
+show getStatusFullName
+e1
+show getStatusFullName
+e2
+show getStatusFullName
+e1
+show getStatusFullName
+e1
+show getStatusFullName
+e1
+show getStatusFullName
+e3
+show getStatusFullName
+e4
+show getStatusFullName
+e4
+show getStatusFullName
+e1
+show getStatusFullName
+e2
+show getStatusFullName
+e2
+show getStatusFullName
+e4
+show getStatusFullName
+e1
+show getStatusFullName
+e2
+show getStatusFullName
+e4
+show getStatusFullName
+e4
+show getStatusFullName
+e5
+show getStatusFullName
+e1
+show getStatusFullName
+e2
+show getStatusFullName
+e4
+show getStatusFullName
+e1
+show getStatusFullName
+e5
+show getStatusFullName
+e2
+show getStatusFullName
+e1
+show getStatusFullName
+e4
+show getStatusFullName
+e3
+show getStatusFullName
+e5
+show getStatusFullName
