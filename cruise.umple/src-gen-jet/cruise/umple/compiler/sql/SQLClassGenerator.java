68da22c72d16dbca861f8af37c70ea6a8f5b6a01
Adam Dzialoszynski <w12ucosp@gmail.com>
2012-04-07 01:20:21 +0000

    Commit includes all recent changes to the SQL generation. -CREATE DATABASE & CREATE TABLE are now IF NOT EXISTS to prevent SQL errors. -Primary Keys are correctly resolved and output now. Previously, there could be errors when the Umple Key was not well defined. -Foreign key constraints were moved to a new jet file. This file (members_AllForeignKeys.jet) is unused right now but contains the required code to get the job done. -Probably more...
    git-svn-id: http://umple.googlecode.com/svn/trunk@1678 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-jet/cruise/umple/compiler/sql/SQLClassGenerator.java
+++ cruise.umple/src-gen-jet/cruise/umple/compiler/sql/SQLClassGenerator.java
@@ -22,10 +22,10 @@ public class SQLClassGenerator implements ILang
   protected final String TEXT_2 = NL;
   protected final String TEXT_3 = NL;
   protected final String TEXT_4 = NL;
-  protected final String TEXT_5 = NL + "CREATE TABLE `";
-  protected final String TEXT_6 = "`";
-  protected final String TEXT_7 = " (" + NL;
-  protected final String TEXT_8 = NL + "  " + NL + ");";
+  protected final String TEXT_5 = NL + "CREATE TABLE IF NOT EXISTS `";
+  protected final String TEXT_6 = "`" + NL + "(";
+  protected final String TEXT_7 = "  ";
+  protected final String TEXT_8 = NL + ");";
   protected final String TEXT_9 = NL;
 
   // Add a newline to the end of the input
@@ -49,12 +49,6 @@ public class SQLClassGenerator implements ILang
     final StringBuffer stringBuffer = new StringBuffer();
     stringBuffer.append(TEXT_1);
     
-/*
-A brief outline of what needs to be done:
-
-Associations, Auto-unique variables
-
-*/
 
   UmpleClass uClass = (UmpleClass) uElement;
   GeneratedClass gClass = uClass.getGeneratedClass();
@@ -90,19 +84,61 @@ Associations, Auto-unique variables
      if (uClass.getPackageName() != "") { append(stringBuffer, "{0}`.`", gen.translate("packageName", uClass));} 
     stringBuffer.append(gen.translate("type", uClass) );
     stringBuffer.append(TEXT_6);
-    stringBuffer.append( gen.translate("isA",uClass) /* a bit of work needs to be done here, if we inherit, do we just add those attributes to the table?*/ );
+    
+
+/*
+This file creates the necessary attributes that the class will need for a successful foreign key to another class (association)
+It creates local copies of the foreign class' private key attributes. 
+Note: The foreign key constraint is not defined here, as the associated class may not be defined yet. See the members_AllForeignKeys.jet file for those.
+*/
+  appendln(stringBuffer, "");
+  appendln(stringBuffer, "  /*------------------------*/");
+  appendln(stringBuffer, "  /* MEMBER VARIABLES       */");
+  appendln(stringBuffer, "  /*------------------------*/");
+  
+  isFirst = true;
+  
+  for (AssociationVariable av : uClass.getAssociationVariables())
+  {
+    if (!av.getIsNavigable())
+    {
+      continue;
+    }
+    
+    String attribute = av.isOne() ? gen.translate("attributeOne",av) : gen.translate("attributeMany",av);
+ 
+    if (av.isOptionalOne() || av.isOnlyOne() || av.isN())
+    { 
+      if (isFirst)
+      {
+        appendln(stringBuffer, "");
+        appendln(stringBuffer,"  /*{0} Associations*/", gen.translate("type", uClass));
+        isFirst = false;
+      }
+      UmpleClass associatedUClass = av.getRelatedAssociation().getUmpleClass();
+      List<Attribute> foreignPKList = gen.resolvePrimaryKey(associatedUClass);
+      for(int i = 1; i <= av.getMultiplicity().getUpperBound(); i += 1)
+      { 
+	    for(Attribute foreignPK : foreignPKList)
+	    {
+	      appendln(stringBuffer, "  {0}_{1}{2} {3}{4},", gen.translate("type", associatedUClass),
+	                                                       gen.translate("attributeOne", foreignPK), 
+	                                                       av.isN() ? "_" + i : "", 
+	                                                       gen.translate("type", foreignPK),
+	                                                       av.isN() ? " NOT NULL" : "");
+	    }
+      }
+    }
+  }
+
     stringBuffer.append(TEXT_7);
     
   
-  // SQL doesn't have a notion of a static 'member' or variable,
-  // however, it may be useful to document static members in the code
-  // so this template has been left as is.
+  /*
+  This file declares constant attributes for tables. 
+  The "constant" requirement is enforced with a check statement.
+  */
   
-  if (uClass.getIsSingleton())
-  {
-    append(stringBuffer, "  include Singleton");
-  }
-
   isFirst = true;
   for(Attribute av : uClass.getAttributes())
   {
@@ -115,33 +151,20 @@ Associations, Auto-unique variables
     if (isFirst)
     {
       appendln(stringBuffer, "");
-      appendln(stringBuffer, "");
-      appendln(stringBuffer, "  /*-----------------------*/");
-      appendln(stringBuffer, "  /* STATIC VARIABLES      */");
-      appendln(stringBuffer, "  /*-----------------------*/");
+      appendln(stringBuffer,"  /*{0} Constants*/", gen.translate("type", uClass));      
       isFirst = false;
     }
   
-    appendln(stringBuffer, "");
     appendln(stringBuffer, "  {0} {1} DEFAULT '{2}' CHECK ({1} = '{2}'),", gen.translate("attributeConstant",av), gen.translate("type",av), gen.translate("parameterValue",av));
   }
   
 
     
 {
-  appendln(stringBuffer, "");
-  appendln(stringBuffer, "");
-  appendln(stringBuffer, "  /*------------------------*/");
-  appendln(stringBuffer, "  /* MEMBER VARIABLES       */");
-  append(stringBuffer, "  /*------------------------*/");
 
-  //Check for Primary Key
-  boolean noKey = false;
-  String genKeyName = gen.translate("type", uClass) + "_id";
-  if (uClass.getKey().getMembers().length == 0)
-  {
-  	noKey = true;
-  }
+/*
+This file generates SQL attributes for a given class. Once all the attributes have been defined, it generates the SQL Primary Key for the table.
+*/
 
   isFirst = true;
   for(Attribute av : uClass.getAttributes())
@@ -153,13 +176,7 @@ Associations, Auto-unique variables
     if (isFirst)
     {
       appendln(stringBuffer, "");
-      appendln(stringBuffer, "");
       appendln(stringBuffer,"  /*{0} Attributes*/", gen.translate("type", uClass));      
-      if (noKey)
-      {
-        appendln(stringBuffer, "  /*Warning: No Primary Key specified. {0} was generated.*/", genKeyName);
-        appendln(stringBuffer, "  {0} INT AUTO_INCREMENT,", genKeyName);
-      }
       isFirst = false;
     }
     
@@ -177,90 +194,33 @@ Associations, Auto-unique variables
     {
    	  append(stringBuffer, " AUTO_INCREMENT");
     }
-    else if (gen.translate("parameterValue",av).compareToIgnoreCase("null") != 0)
+    if (gen.translate("parameterValue",av).compareToIgnoreCase("null") != 0)
     {
    	  append(stringBuffer, " DEFAULT '{0}'", gen.translate("parameterValue",av));
     }
+    /* Unique not implemented yet.
+    if (av.getIsUnique())
+    {
+   	  append(stringBuffer, " UNIQUE");
+    }
+    */
         
     appendln(stringBuffer, ",");
   }
   
   
   //Primary Key Generation
-  if (!noKey)
+  String tKey = "";
+  List<Attribute> PKList = gen.resolvePrimaryKey(uClass);
+  for(Attribute PK : PKList)
   {
-  	String tKey = "";
-  	for(String key : uClass.getKey().getMembers())
-  	{
-  	  if (uClass.getAttribute(key) == null) //Check if the given key is a constant.
-  	  {
-  	    tKey += "/*Warning: Constant attribute in Primary Key*/ ";
-  	  }
-	  tKey += StringFormatter.toUnderscore(key) + ", ";
-  	}
-  	//Remove trailing comma
-  	tKey = tKey.substring(0, tKey.length()-2);
-  	appendln(stringBuffer, "  PRIMARY KEY({0}),", tKey);
+    tKey += gen.translate("attributeOne", PK) + ", ";
   }
-  else
-  {
-  	appendln(stringBuffer, "  PRIMARY KEY({0}),", genKeyName);
-  }
+  //Remove trailing comma
+  tKey = tKey.substring(0, tKey.length()-2);
+  appendln(stringBuffer, "  PRIMARY KEY({0})", tKey);
+  
 }
-
-    
-  /*
-   This class needs a lot of work, for every possible association type,
-   a relationship between the different tables must be established.
-   
-   You may want to break these into separate files as the other projects do, 
-   I.e:
-    association_ManyToOne.jet
-    association_MNToMany.jet 
-    etc...
-  */
-  isFirst = true;
-  for (AssociationVariable av : uClass.getAssociationVariables())
-  {
-    if (!av.getIsNavigable())
-    {
-      continue;
-    }
-    
-    String attribute = av.isOne() ? gen.translate("attributeOne",av) : gen.translate("attributeMany",av);
- 
-    if (isFirst)
-    {
-      appendln(stringBuffer, "");
-      appendln(stringBuffer, "");
-      appendln(stringBuffer,"  /*{0} Associations*/", gen.translate("type", uClass));
-      isFirst = false;
-    }
-    
-    if (av.isN() || av.isMandatoryOne())
-    { 
-      UmpleClass associatedUClass = av.getRelatedAssociation().getUmpleClass();
-      for(int i = 0; i < av.getMultiplicity().getUpperBound(); i += 1)
-      { 
-	    for(String foreignPrimaryKeyName : associatedUClass.getKey().getMembers())
-	    {
-	      Attribute foreignPK = associatedUClass.getAttribute(foreignPrimaryKeyName);
-	      if (foreignPK == null) //Check if the given key is a constant.
-  	      {
-  	        append(stringBuffer, "/*Warning: Constant attribute in associated class' Primary Key*/ ");
-  	      }
-  	      else
-  	      {
-	        appendln(stringBuffer, "  {0}_{1}{2} {3},", gen.translate("type", associatedUClass),
-	                                                    gen.translate("attributeOne", foreignPK), 
-	                                                    av.isN() ? "_" + i : "", 
-	                                                    gen.translate("type", foreignPK));
-	      }
-	    }
-      }
-    }
-    
-  }
 
     stringBuffer.append(TEXT_8);
     stringBuffer.append(TEXT_9);
