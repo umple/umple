db8e939f682944fbfb86cf38179de128357b1917
Andrew Forward <aforward@gmail.com>
2012-05-22 14:22:35 +0000

    Removing the elections php project
    git-svn-id: http://umple.googlecode.com/svn/trunk@1743 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- examples/elections_php_executable/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$db_hostname='localhost:3306;
-	$db_username='umple';
-	$db_password='ems';
-?>
--- examples/elections_php_executable/openpollindex.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include("OpenPoll.php");
-	include("Credentials.php");
-
-	$openpoll = new OpenPoll(Credentials::$db_hostname,Credentials::$db_username,Credentials::$db_password);
-?>
