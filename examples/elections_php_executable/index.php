8799dc9a7e164e3eff96d52d201359c0276aadb9
Ali Fatolahi <ali.fatolahi@gmail.com>
2012-05-23 11:02:17 +0000

    fixing php version of Elections
    git-svn-id: http://umple.googlecode.com/svn/trunk@1762 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- examples/elections_php_executable/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//include("Controller.php");
-	//include("Credentials.php");
-
-	//$controller = new Controller(Credentials::$db_hostname,Credentials::$db_username,Credentials::$db_password);
-?>
