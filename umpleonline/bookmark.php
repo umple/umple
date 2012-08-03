1012cdf7e1b1166f202455ca0458acad41ed1a73
Timothy Lethbridge <timothyclethbridge@gmail.com>
2012-08-03 17:27:32 +0000

    Ensuring that all extra directories are deleted when bookmarks are hit
    git-svn-id: http://umple.googlecode.com/svn/trunk@1968 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- umpleonline/bookmark.php
+++ umpleonline/bookmark.php
@@ -15,15 +15,33 @@ $tempModelId = $_REQUEST["model"];
 date_default_timezone_set('UTC');
 $savedModel = nextFilename("ump",date("ymd"));
 
-
 $saveModelId = extractModelId($savedModel);
 
 $filename = "ump/{$tempModelId}/model.ump";
 
+// If boomkarking is being attempted by a crawler that has no javascript
+// and has just created a model.ump file, then this is an error.
+// It is also an error if an attempt is made to later on bookmark some
+// file that has long since been deleted
 if (!is_file($filename))
 {
   header('HTTP/1.0 404 Not Found');
   readfile('../404.shtml');
+  if(substr($tempModelId,0,3) == "tmp") {
+    recursiveDelete("ump/{$tempModelId}");
+  }
+  recursiveDelete("ump/{$saveModelId}");
+  exit();
+}
+
+if (!is_file("ump/{$tempModelId}/model.ump.erroroutput"))
+{
+  header('HTTP/1.0 412 Precondition Failed');
+  echo "<html><head><title>Javascript Off</title></head><body><p>You cannot make a bookmarked page when JavaScript is turned off. Please turn it on.</p></body></html>";
+  if(substr($tempModelId,0,3) == "tmp") {
+    recursiveDelete("ump/{$tempModelId}");
+  }
+  recursiveDelete("ump/{$saveModelId}");
   exit();
 }
 
