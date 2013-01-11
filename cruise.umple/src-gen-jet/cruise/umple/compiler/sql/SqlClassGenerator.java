1922a4a247d3f415d3e282bf1b2fa706b741937c
aforward@gmail.com <aforward@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2013-01-11 21:06:04 +0000

    Fixed the versionning number in our files back to the last published version, 1.16.0.2388
    git-svn-id: http://umple.googlecode.com/svn/trunk@2406 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-jet/cruise/umple/compiler/sql/SqlClassGenerator.java
+++ cruise.umple/src-gen-jet/cruise/umple/compiler/sql/SqlClassGenerator.java
@@ -18,7 +18,7 @@ public class SqlClassGenerator implements ILang
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "-- PLEASE DO NOT EDIT THIS CODE" + NL + "-- This code was generated using the UMPLE ${last.version} modeling language!";
+  protected final String TEXT_1 = "-- PLEASE DO NOT EDIT THIS CODE" + NL + "-- This code was generated using the UMPLE 1.16.0.2388 modeling language!";
   protected final String TEXT_2 = NL;
   protected final String TEXT_3 = NL;
   protected final String TEXT_4 = NL + "CREATE TABLE IF NOT EXISTS ";
