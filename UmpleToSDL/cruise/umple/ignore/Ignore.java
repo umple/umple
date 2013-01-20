99359c568df0f055a4487dc064f2a43a043d56fe
Miguel Garzon <asopapos@gmail.com>
2013-01-20 22:11:09 +0000

    UMPLE TO SDL
    git-svn-id: http://umple.googlecode.com/svn/trunk@2451 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ UmpleToSDL/cruise/umple/ignore/Ignore.java
@@ -0,0 +1,25 @@
+package cruise.umple.ignore;
+
+public class Ignore
+{
+  protected static String nl;
+  public static synchronized Ignore create(String lineSeparator)
+  {
+    nl = lineSeparator;
+    Ignore result = new Ignore();
+    nl = null;
+    return result;
+  }
+
+  public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
+  protected final String TEXT_1 = "-- PLEASE DO NOT EDIT THIS CODE" + NL + "-- This code was generated using the UMPLE @UMPLE_VERSION@ modeling language!";
+  protected final String TEXT_2 = NL;
+
+  public String generate(Object argument)
+  {
+    final StringBuffer stringBuffer = new StringBuffer();
+    stringBuffer.append(TEXT_1);
+    stringBuffer.append(TEXT_2);
+    return stringBuffer.toString();
+  }
+}
