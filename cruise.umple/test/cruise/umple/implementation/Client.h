cf680d116af9d13bc31359898cad29bfbd194b54
Quinlan Jung <quinlanjung@gmail.com>
2013-04-08 11:31:17 +0000

    added rest of preconditions + tests
    git-svn-id: http://umple.googlecode.com/svn/trunk@2681 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/implementation/Client.h
+++ cruise.umple/test/cruise/umple/implementation/Client.h
@@ -7,7 +7,6 @@
 #include <string>
 using namespace std;
 
-// attributes on both sides of the constraint's boolean expression
 class Client
 {
    //------------------------
@@ -22,14 +21,13 @@ class Client
 
   //Client Attributes
   int minAge;
-  int age;
 
   //------------------------
   // Constructor
   //------------------------
   public:
 
-   Client(const int & aMinAge, const int & aAge);
+   Client(const int & aMinAge);
 
   
  Client(const Client & client);
@@ -46,16 +44,15 @@ class Client
   //------------------------
 
   bool setMinAge(const int & aMinAge);
-  bool setAge(const int & aAge);
   int getMinAge() const;
-
-  int getAge() const;
 
   //------------------------
   // Destructor
   //------------------------
 virtual ~Client();
 
+
+   int someMethod(Integer arg);
 };
 
 #endif
