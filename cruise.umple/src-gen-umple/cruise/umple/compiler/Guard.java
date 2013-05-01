590db4a78c7f657f5d3bcfd4fa0f5923b8128f4f
Timothy Lethbridge <timothyclethbridge@gmail.com>
2013-05-01 12:03:38 +0000

    Gen files to 2718
    git-svn-id: http://umple.googlecode.com/svn/trunk@2722 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-umple/cruise/umple/compiler/Guard.java
+++ cruise.umple/src-gen-umple/cruise/umple/compiler/Guard.java
@@ -1,5 +1,5 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE ${last.version} modeling language!*/
+/*This code was generated using the UMPLE 1.17.0.2716 modeling language!*/
 
 package cruise.umple.compiler;
 import cruise.umple.compiler.Position;
