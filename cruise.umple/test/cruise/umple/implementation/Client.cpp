617e960390306206e4091bd6a1de374ce876238a
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2013-06-28 02:23:48 +0000

    Adding missing files to build
    git-svn-id: http://umple.googlecode.com/svn/trunk@2844 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/implementation/Client.cpp
+++ cruise.umple/test/cruise/umple/implementation/Client.cpp
@@ -4,14 +4,24 @@
 
 #include "Client.h"
 
+// attributes on both sides of the constraint's boolean expression
 	
   //------------------------
   // CONSTRUCTOR
   //------------------------
   
- Client::Client(const int & aMinAge)
+ Client::Client(const int & aMinAge, const int & aAge)
   {
+    if ( !(aAge>aMinAge))
+    {
+      throw "Please provide a valid age";
+    }
+    if ( !(aAge>aMinAge))
+    {
+      throw "Please provide a valid minAge";
+    }
     minAge = aMinAge;
+    age = aAge;
   }
   
   //------------------------
@@ -21,6 +31,7 @@
  Client::Client(const Client & client)
   {
     this->minAge = client.minAge;
+    this->age = client.age;
   }
   	
   //------------------------
@@ -30,6 +41,7 @@
  Client Client::operator=(const Client & client)
   {
     this->minAge = client.minAge;
+    this->age = client.age;
   }
 
   //------------------------
@@ -39,14 +51,33 @@
   bool Client::setMinAge(const int & aMinAge)
   {
     bool wasSet = false;
+    if (age>aMinAge)
+    {
     minAge = aMinAge;
     wasSet = true;
+    }
+    return wasSet;
+  }
+
+  bool Client::setAge(const int & aAge)
+  {
+    bool wasSet = false;
+    if (aAge>minAge)
+    {
+    age = aAge;
+    wasSet = true;
+    }
     return wasSet;
   }
 
   int Client::getMinAge() const
   {
     return minAge;
+  }
+
+  int Client::getAge() const
+  {
+    return age;
   }
 
   
@@ -56,18 +87,4 @@
   
 Client::~Client()
   {}
-
-   int Client::someMethod(int arg){
-
-      if( !(arg>5))
-        {
-        throw "Please provide a valid arg";
-        }
-      if( !(minAge<8))
-        {
-        throw "Please provide a valid minAge";
-        }
-      // rest of stuff that we don't interpret
- return 0;
-  }
 
