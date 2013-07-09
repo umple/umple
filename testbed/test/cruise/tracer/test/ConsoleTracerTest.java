3841bccbf2fbbb9a96b6b6229e14043db834b8f2
Hamoud Aljamaan <hjamaan@gmail.com>
2013-07-09 05:46:01 +0000

    [trace] trace tests
    git-svn-id: http://umple.googlecode.com/svn/trunk@2881 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- testbed/test/cruise/tracer/test/ConsoleTracerTest.java
+++ testbed/test/cruise/tracer/test/ConsoleTracerTest.java
@@ -29,15 +29,27 @@ public class ConsoleTracerTest
 			int index=0;
 			String[] expected = {"Time",
 					"at_s,name,null,Geoff",
-					"at_s,name,Geoff,Hamoud"};
+					"at_s,name,Geoff,Hamoud",
+					"at_s,number1,7,8",
+					"at_s,number2,22",
+					"at_s,number2,33",
+					"at_s,number3,45",
+					"at_s,number3,46",
+					"at_s,number4,55",
+					"at_s,number4,66",
+					"at_s,number5,0,1",
+					"at_s,number5,1,2",
+					"at_s,number5,2,3"
+					};
 			Integer[] testField = {0,9,9};
 			@Override
 			public void println(String x){
 				if(index<expected.length){
-									
+					
+					System.out.println("out = "+x);
 					String[] actualOutput = x.split(",");
 					String[] expectedOutput = expected[index].split(",");
-					
+				
 					// header skip
 					if( index != 0 )
 						for( int i = 0 ; i < expectedOutput.length ; ++i )
@@ -52,10 +64,35 @@ public class ConsoleTracerTest
 		};
 	  System.setErr(ps);
 	  
-	  ConsoleTracerTester cTest = new ConsoleTracerTester(null);
+	  ConsoleTracerTesterA cTest = new ConsoleTracerTesterA(null, 0, null, 0, 0, 0, 0, 0);
 	  
 	  cTest.setName("Geoff");
 	  cTest.setName("Hamoud");
+	  cTest.getName();
+	  
+	  cTest.setNumber1(7);
+	  cTest.setNumCond(110);
+	  cTest.setNumber1(8);
+	  
+	  cTest.setNumber2(11);
+	  cTest.setNumCond(20);
+	  cTest.setNumber2(22);
+	  cTest.setNumber2(33);
+	  
+	  cTest.setNumber3(44);
+	  cTest.setNumCond(1);
+	  cTest.setNumber3(45);
+	  cTest.setNumber3(46);
+
+	  cTest.setNumber4(55);
+	  cTest.setNumber4(66);
+	  cTest.setNumCond(-99);
+	  cTest.setNumber4(77);
+	  
+	  cTest.setNumber5(1);
+	  cTest.setNumber5(2);
+	  cTest.setNumber5(3);
+	  cTest.setNumber5(4);
 	  
   }
   
---
+++ testbed_php/src-gen-umple/ConsoleTracerTesterA.php
@@ -0,0 +1,201 @@
+<?php
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.17.0.2716 modeling language!*/
+
+class ConsoleTracerTesterA
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //ConsoleTracerTesterA Attributes
+  private $name;
+
+  /**
+   * address;
+   * trace get address;
+   * Trace attributes with conditions
+   */
+  private $numCond;
+
+  /**
+   * ToBe added string constrints
+   */
+  private $strCond;
+  private $number1;
+  private $number2;
+  private $number3;
+  private $number4;
+  private $number5;
+  private $traceFlag1;
+  private $traceFlag2;
+  private $traceFlag3;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public function __construct($aName, $aNumCond, $aStrCond, $aNumber1, $aNumber2, $aNumber3, $aNumber4, $aNumber5)
+  {
+    $this->name = $aName;
+    $this->numCond = $aNumCond;
+    $this->strCond = $aStrCond;
+    $this->number1 = $aNumber1;
+    $this->number2 = $aNumber2;
+    $this->number3 = $aNumber3;
+    $this->number4 = $aNumber4;
+    $this->number5 = $aNumber5;
+    $this->traceFlag1 = false;
+    $this->traceFlag2 = true;
+    $this->traceFlag3 = 0;
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public function setName($aName)
+  {
+    $wasSet = false;
+    ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,10,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,name,".$this->name.",".$aName );
+    $this->name = $aName;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumCond($aNumCond)
+  {
+    $wasSet = false;
+    $this->numCond = $aNumCond;
+    $wasSet = true;
+      if(@numCond==1) then
+        
+        traceFlag1=true;
+         end
+      if(@numCond<=0) then
+        
+        traceFlag2=false;
+         end
+    return $wasSet;
+  }
+
+  public function setStrCond($aStrCond)
+  {
+    $wasSet = false;
+    $this->strCond = $aStrCond;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumber1($aNumber1)
+  {
+    $wasSet = false;
+      if($numCond>100)
+        {
+        ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,26,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,number1,".$this->number1.",".$aNumber1 );
+        }
+    $this->number1 = $aNumber1;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumber2($aNumber2)
+  {
+    $wasSet = false;
+    $this->number2 = $aNumber2;
+      if($numCond==20)
+        {
+        ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,27,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,number2,".$this->number2 );
+        }
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumber3($aNumber3)
+  {
+    $wasSet = false;
+    $this->number3 = $aNumber3;
+      if($traceFlag1)
+        {
+        ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,28,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,number3,".$this->number3 );
+        }
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumber4($aNumber4)
+  {
+    $wasSet = false;
+    $this->number4 = $aNumber4;
+      if($traceFlag2)
+        {
+        ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,29,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,number4,".$this->number4 );
+        }
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function setNumber5($aNumber5)
+  {
+    $wasSet = false;
+      if($traceFlag3<3)
+        {
+        ConsoleTracer::handle( gettimeofday(true).",".uniqid().",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed_php/src/../../testbed/src/TestHarnessTracer.ump,30,ConsoleTracerTesterA,".spl_object_hash( $this ).",at_s,number5,".$this->number5.",".$aNumber5 );
+      traceFlag3++;
+        }
+    $this->number5 = $aNumber5;
+    $wasSet = true;
+    return $wasSet;
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function getNumCond()
+  {
+    return $this->numCond;
+  }
+
+  public function getStrCond()
+  {
+    return $this->strCond;
+  }
+
+  public function getNumber1()
+  {
+    return $this->number1;
+  }
+
+  public function getNumber2()
+  {
+    return $this->number2;
+  }
+
+  public function getNumber3()
+  {
+    return $this->number3;
+  }
+
+  public function getNumber4()
+  {
+    return $this->number4;
+  }
+
+  public function getNumber5()
+  {
+    return $this->number5;
+  }
+
+  public function equals($compareTo)
+  {
+    return $this == $compareTo;
+  }
+
+  public function delete()
+  {}
+
+}
+?>
