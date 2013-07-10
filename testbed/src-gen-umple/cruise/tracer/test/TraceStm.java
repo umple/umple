8d593aabd7124e69bbf3e5849e2b13cb5aa33d0e
Hamoud Aljamaan <hjamaan@gmail.com>
2013-07-10 20:17:10 +0000

    [trace] trace tests + fixing a bug
    git-svn-id: http://umple.googlecode.com/svn/trunk@2890 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ testbed/src-gen-umple/cruise/tracer/test/TraceStm.java
@@ -0,0 +1,151 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.17.0.2716 modeling language!*/
+
+package cruise.tracer.test;
+import cruise.util.ConsoleTracer;
+import java.util.Date;
+
+// line 7 "../../../../src/TestHarnessTracer.ump"
+public class TraceStm
+{
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+  public @interface umplesourcefile{int[] line();String[] file();int[] javaline();int[] length();}
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //TraceStm Attributes
+  private String log;
+
+  //TraceStm State Machines
+  enum Status { Open, Close, HalfOpen, HalfClose }
+  private Status status;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public TraceStm()
+  {
+    log = "initial value";
+    setStatus(Status.Open);
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setLog(String aLog)
+  {
+    boolean wasSet = false;
+    log = aLog;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getLog()
+  {
+    return log;
+  }
+
+  public String getStatusFullName()
+  {
+    String answer = status.toString();
+    return answer;
+  }
+
+  public Status getStatus()
+  {
+    return status;
+  }
+
+  public boolean anEvent()
+  {
+    boolean wasEventProcessed = false;
+    
+    Status aStatus = status;
+    switch (aStatus)
+    {
+      case Open:
+        exitStatus();
+    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_t,Open,anEvent,Close" );
+        setStatus(Status.Close);
+        wasEventProcessed = true;
+        break;
+      case Close:
+    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_t,Close,anEvent,Open" );
+        setStatus(Status.Open);
+        wasEventProcessed = true;
+        break;
+      default:
+        // Other states do respond to this event
+    }
+
+    return wasEventProcessed;
+  }
+
+  public boolean ev2()
+  {
+    boolean wasEventProcessed = false;
+    
+    Status aStatus = status;
+    switch (aStatus)
+    {
+      case HalfOpen:
+    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_t,HalfOpen,ev2,HalfClose" );
+        setStatus(Status.HalfClose);
+        wasEventProcessed = true;
+        break;
+      case HalfClose:
+    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_t,HalfClose,ev2,HalfOpen" );
+        setStatus(Status.HalfOpen);
+        wasEventProcessed = true;
+        break;
+      default:
+        // Other states do respond to this event
+    }
+
+    return wasEventProcessed;
+  }
+
+  @umplesourcefile(line={14}, file={"TestHarnessTracer.ump"}, javaline={120}, length={1})
+  private void exitStatus()
+  {
+    switch(status)
+    {
+      case Open:    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_x,Open,status" );
+
+        // line 14 "../../../../src/TestHarnessTracer.ump"
+        log = "exit called";
+        break;
+    }
+  }
+
+  private void setStatus(Status aStatus)
+  {
+    status = aStatus;
+
+    // entry actions and do activities
+    switch(status)
+    {
+      case Open:    ConsoleTracer.handle( System.currentTimeMillis()+","+Thread.currentThread().getId()+",/Users/hjamaan/Documents/uOttawa/GoogleUmple/trunk/testbed/src/TestHarnessTracer.ump,29,TraceStm,"+System.identityHashCode(this)+",sm_e,Open,status" );
+
+        // line 13 "../../../../src/TestHarnessTracer.ump"
+        log = "entry called";
+        break;
+    }
+  }
+
+  public void delete()
+  {}
+
+
+  public String toString()
+  {
+	  String outputString = "";
+    return super.toString() + "["+
+            "log" + ":" + getLog()+ "]"
+     + outputString;
+  }
+}
