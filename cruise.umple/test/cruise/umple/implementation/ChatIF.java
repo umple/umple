27075fdc4fd147c4243bb92377f80715d7e966f5
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2013-09-17 01:00:26 +0000

    patch-JCCharbonneau-Issue373-VoidImplementedMethod.diff
    git-svn-id: http://umple.googlecode.com/svn/trunk@2956 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umple/test/cruise/umple/implementation/ChatIF.java
@@ -0,0 +1,11 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.17.0.2716 modeling language!*/
+
+
+public interface ChatIF
+{
+  
+  // ABSTRACT METHODS 
+
+ public void display();
+}
---
+++ cruise.umple/test/cruise/umple/implementation/ClientConsole.java
@@ -0,0 +1,36 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.17.0.2716 modeling language!*/
+
+
+
+// line 5 "ClassTemplateTest_VoidImplementedMethod.ump"
+public class ClientConsole implements ChatIF
+{
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+  public @interface umplesourcefile{int[] line();String[] file();int[] javaline();int[] length();}
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public ClientConsole()
+  {}
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {}
+
+  @umplesourcefile(line={2},file={"ClassTemplateTest_VoidImplementedMethod.ump"},javaline={31},length={2})
+  @Override
+  public void display(){
+          return ;
+  }
+
+}
