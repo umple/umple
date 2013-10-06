1987d6bce922460a043606f1ab45ef087ccea98c
Tiago Nascimento <atojet@gmail.com>
2013-10-06 06:21:47 +0000

    [uigu2] Object creation accepting Associations
    git-svn-id: http://umple.googlecode.com/svn/trunk@3092 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-uigu2/kissmvc_uigu2.php
+++ cruise.umple/src-uigu2/kissmvc_uigu2.php
@@ -31,22 +31,47 @@ class Uigu2_Controller extends KISS_Controller {
 
   /*
   * Creates an object of an Umple domain class and registers it in the Session
-  * under the execution instance given by $execution_id. Make sure session_start()
-  * is called externally prior to using this function.
+  * under the execution instance given by $execution_id. Returns true if creation was succesful.
   */
   public function create_object($class_name, $params){
+    //TODO: test if __autoload() is declared
+    //TODO: test if session was started
     $execution_id = $this->get_execution_id();
-    //relies on the autoload function declared externally, see index.php
+    $umple_class = $this->umple_model['ELEMENTS'][$class_name];
+    $constructor = $umple_class['constructor'];
+    $associations = $umple_class['associations'];
+
+    if(count($params) != count($constructor)){
+      //TODO: set error message
+      return false;
+    }
+    //if constructor parameter is an association, value in $param is the index to obtain the object and must be replaced by it
+    for($i = 0; $i < count($constructor); $i++){
+      if(isset($associations[$constructor[$i]['name']])){
+        $obj_ref = $this->get_object($constructor[$i]['type'], $params[$i]);
+        if(empty($obj_ref)){
+          //TODO:object not found - set error message
+          return false;
+        }else{
+          $params[$i] = $obj_ref;
+        }
+      }
+    }
     $reflection = new ReflectionClass($class_name);
     $_SESSION[$execution_id]['instances'][$class_name][] = $reflection->newInstanceArgs($params);
+    return true;
   }
 
   /*
-  * Returns a specific Umple Object already instanciated
+  * Returns a specific Umple Object already instanciated, or false if no such object was found.
   */
   public function get_object($class_name, $index){
     $execution_id = $this->get_execution_id();
-    return $_SESSION[$execution_id]['instances'][$class_name][$index];
+    if($obj = $_SESSION[$execution_id]['instances'][$class_name][$index]){
+      return $obj;
+    }else{
+      return false;
+    }
   }
 
   /*
@@ -60,8 +85,8 @@ class Uigu2_Controller extends KISS_Controller {
   * Returns a specific Umple Element declared in $umple_model, or false if it doesn't exist 
   */
   public function get_element($element_name){
-    if(isset($this->umple_model['ELEMENTS'][$element_name])){
-      return $this->umple_model['ELEMENTS'][$element_name];
+    if($element = $this->umple_model['ELEMENTS'][$element_name]){
+      return $element;
     }else {
       return false;
     }
