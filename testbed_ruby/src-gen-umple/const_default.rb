9786842a3f86a415c0e058cfa3ce748315dac7e2
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2013-11-28 20:12:19 +0000

    patch-Aliaa-QSM-QueuedWithConcurrentAndNestedStateMachines.diff
    git-svn-id: http://umple.googlecode.com/svn/trunk@3244 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ testbed_ruby/src-gen-umple/const_default.rb
@@ -0,0 +1,48 @@
+# PLEASE DO NOT EDIT THIS CODE
+# This code was generated using the UMPLE 1.18.0.3036 modeling language!
+# NOTE: Ruby generator is experimental and is missing some features available in
+# in other Umple generated languages like Java or PHP
+
+module CruiseAttributesTest
+require 'date'
+require 'time'
+
+class ConstDefault
+
+
+  #------------------------
+  # STATIC VARIABLES
+  #------------------------
+
+  I1 = 0;
+  I2 = 0;
+  D1 = 0.0;
+  D2 = 0.0;
+  F1 = 0.0;
+  F2 = 0.0;
+  B1 = false;
+  B2 = false;
+  STR = "";
+  DATE = Date.parse("2013-11-28");
+  TIME = Time.parse("00:00:00");
+
+  #------------------------
+  # CONSTRUCTOR
+  #------------------------
+
+  def initialize()
+    @initialized = false
+    @deleted = false
+    @initialized = true
+  end
+
+  #------------------------
+  # INTERFACE
+  #------------------------
+
+  def delete
+    @deleted = true
+  end
+
+end
+end
