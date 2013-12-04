9160705fcc65feadf9cf6825849bb6b0d4875c58
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2013-12-04 00:37:06 +0000

    Updating prototype.js to improve UmpleOnline
    git-svn-id: http://umple.googlecode.com/svn/trunk@3268 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- testbed_ruby/src-gen-umple/const_default.rb
+++ testbed_ruby/src-gen-umple/const_default.rb
@@ -23,7 +23,7 @@ class ConstDefault
   B1 = false;
   B2 = false;
   STR = "";
-  DATE = Date.parse("2013-12-01");
+  DATE = Date.parse("2013-12-03");
   TIME = Time.parse("00:00:00");
 
   #------------------------
