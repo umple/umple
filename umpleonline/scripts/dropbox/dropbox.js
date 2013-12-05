79e866b242bf5f5e1af796aa2bbda6e9740d6e11
Kenan Kigunda <kkigunda@gmail.com>
2013-12-05 21:45:22 +0000

    Dropbox integration: allow for multiple saves
    git-svn-id: http://umple.googlecode.com/svn/trunk@3274 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- umpleonline/scripts/dropbox/dropbox.js
+++ umpleonline/scripts/dropbox/dropbox.js
@@ -1,10 +1,14 @@
 jQuery(document).ready(function() {
   jQuery('.dropbox-add-saver')
   .addClass('dropbox-option')
-  .append('<li><span class="dropbox-saver"></span></li>');
+  .append('<li><span class="dropbox-button-saver"></span></li>');
   jQuery('.dropbox-add-chooser')
   .addClass('dropbox-option')
-  .append('<li><input type="dropbox-chooser" class="dropbox-chooser"/></li>');
+  .append('<li><input type="dropbox-chooser" class="dropbox-button-chooser"/></li>');
   DropboxChooser.init();
   DropboxSaver.init();
 });
+
+DropboxMaster = {
+  resetTimeout: 2000,
+};
