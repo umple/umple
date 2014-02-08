c9036fe4f22a7f35ea318a77d9a41b0bce337e3c
aforward@gmail.com <aforward@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-02-08 05:34:44 +0000

    Removed umple generated code
    git-svn-id: http://umple.googlecode.com/svn/trunk@3416 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-umple/cruise/umple/compiler/ILang.java
+++
@@ -1,8 +0,0 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE ${last.version} modeling language!*/
-package cruise.umple.compiler;
-
-public interface ILang
-{
-  public String getCode(UmpleModel model, UmpleElement uElement);
-}
