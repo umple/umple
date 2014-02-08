6293574f54a532a88f1db291581268561efacbc2
Andrew Forward <aforward@gmail.com>
2014-02-08 05:34:44 +0000

    Removed umple generated code
    git-svn-id: http://umple.googlecode.com/svn/trunk@3416 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-umple/cruise/umple/parser/analysis/RuleBasedParserAction.java
+++
@@ -1,8 +0,0 @@
-/*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE ${last.version} modeling language!*/
-package cruise.umple.parser.analysis;
-
-public interface RuleBasedParserAction
-{
-  public void onSuccess(cruise.umple.compiler.Token token);
-}
