3ad021de6794ecb7f503533cc499628cbed866d1
v.abdelzad@gmail.com <v.abdelzad@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-02-11 07:10:49 +0000

    covering state machines in traits and some error checking.
    git-svn-id: http://umple.googlecode.com/svn/trunk@3460 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/compiler/UmpleTraitTest.java
+++ cruise.umple/test/cruise/umple/compiler/UmpleTraitTest.java
@@ -5,6 +5,8 @@ import junit.framework.Assert;
 
 import org.junit.*;
 
+import cruise.umple.compiler.exceptions.UmpleCompilerException;
+import cruise.umple.docs.DocumenterMain.UmpleExceptionHandler;
 import cruise.umple.util.SampleFileWriter;
 
 public class UmpleTraitTest {
@@ -12,29 +14,22 @@ public class UmpleTraitTest {
 	UmpleModel uMode;
 	
 	@Before
-	public void setup() {
-		SampleFileWriter.createFile("traitTest.ump", 
-				"class A { isA T;}"
+	public void setup() {	
+		String  code = "class A { isA T;}"
 				+ "trait T {"
 				+ "depend cruise.umple.util.*;"
 				+ " tName;"
 				+ " tAge;"
 				+ " abstract; singleton; void xFun() {} "
 				+ "[tage>=18]"
-				+ "}");	
-		UmpleFile uFile = new UmpleFile("traitTest.ump");	
-		uMode = new UmpleModel(uFile);
-		try {
-			uMode.run();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}	
+				+ "}"; 	
+		uMode = getRunModel(code);
 	}	
 	@After
 	public void tearDown()
 	{
-	    SampleFileWriter.destroy("traitTest.ump");
-	    SampleFileWriter.destroy("A.java");
+		SampleFileWriter.destroy("traitTest.ump");
+		SampleFileWriter.destroy("A.java");		
 	}
 	
 	@Test 
@@ -78,5 +73,71 @@ public class UmpleTraitTest {
 	public void constraintTest() {
 	//	Assert.assertEquals(1,uMode.getUmpleTrait("T").numberOfConstraints());
 	//	Assert.assertEquals(1,uMode.getUmpleClass("A").numberOfConstraints());
+	}
+	
+	@Test
+	public void selfInheritanceTest() {
+		String code = "class A{isA T;} trait T { isA T;}";
+		UmpleModel model = getModel(code);
+		try {
+			model.run();	
+		} catch (Exception e) {
+			boolean result = e.getMessage().contains("2551");
+			Assert.assertTrue(result);
+		} finally {
+			SampleFileWriter.destroy("traitTest.ump");
+		}	
+	}
+	
+	@Test
+	public void cyclicInheritanceTest() {
+		String code = "class A{isA T;} trait T { isA T1;} trait T1 {isA T;}";
+		UmpleModel model = getModel(code);
+		try {
+			model.run();	
+		} catch (Exception e) {
+			boolean result = e.getMessage().contains("2553");
+			Assert.assertTrue(result);
+		} finally {
+			SampleFileWriter.destroy("traitTest.ump");
+		}	
+	}	
+
+	@Test
+	public void multipleInheritanceTraitsTest() {
+		String code = "class A {isA T1; isA T2;} trait T1 { lazy name1;} trait T2 {lazy name2;}";
+		UmpleModel model = getRunModel(code);
+		Assert.assertEquals(2, model.getUmpleClass("A").numberOfAttributes());		
+	}	
+	
+	@Test
+	public void stateMachineTraitsTest() {
+		String code = "class A {isA T1;} trait T1 { isA T2; status { on { turnOn -> on;}} } trait T2 {sm { active { goActive -> active;}} }";
+		UmpleModel model = getRunModel(code);
+		Assert.assertEquals(2, model.getUmpleClass("A").numberOfStateMachines());		
+	}	
+	
+//-------------------------------------------------------------------------------------	
+//----------------------- Functional methods for this test case -----------------------
+	private UmpleModel getRunModel(String inCode) {
+		SampleFileWriter.createFile("traitTest.ump",inCode);	
+		UmpleFile uFile = new UmpleFile("traitTest.ump");	
+		uMode = new UmpleModel(uFile);
+		try {
+			uMode.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			SampleFileWriter.destroy("traitTest.ump");
+			SampleFileWriter.destroy("A.java");	
+		}
+		return uMode;
+	}
+	
+	private UmpleModel getModel(String inCode) {
+		SampleFileWriter.createFile("traitTest.ump",inCode);	
+		UmpleFile uFile = new UmpleFile("traitTest.ump");	
+		uMode = new UmpleModel(uFile);
+		return uMode;
 	}	
 }
