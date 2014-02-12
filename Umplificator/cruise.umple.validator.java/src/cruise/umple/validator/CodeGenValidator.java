da8a6ea724ee196710ad1e5f07b3554f8a61b6ea
Miguel Garzon <asopapos@gmail.com>
2014-02-12 19:14:46 +0000

    Umplifying the project :) Java sources deleted as in cruise.umple
    git-svn-id: http://umple.googlecode.com/svn/trunk@3465 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- Umplificator/cruise.umple.validator.java/src/cruise/umple/validator/CodeGenValidator.java
+++
@@ -1,8 +0,0 @@
-package cruise.umple.validator;
-
-import java.util.List;
-
-public interface CodeGenValidator {
-	public boolean validateGeneratedCode (String uFile);
-	public boolean validateGeneratedCode (List<String> uFiles);
-}
