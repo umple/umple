2c19ea3ca40891f5677c4229d706e25cc1b3f127
asopapos@gmail.com <asopapos@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-02-13 02:23:18 +0000

    Tests for the validator
    git-svn-id: http://umple.googlecode.com/svn/trunk@3468 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umple.validator/test/cruise/umple/validator/CodeGenMainConsoleTest.java
@@ -0,0 +1,25 @@
+package cruise.umple.validator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cruise.umple.util.SampleFileWriter;
+
+public class CodeGenMainConsoleTest {
+
+	  String pathToInput;
+	  
+	  @Before
+	  public void setUp()
+	  {
+	    pathToInput = SampleFileWriter.rationalize("test/cruise/umple/sequence");
+	  }
+	  
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
