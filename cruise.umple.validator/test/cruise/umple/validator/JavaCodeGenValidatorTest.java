2e4e5b38074ee63ced7ed9eff02fc92e0d25a985
Miguel Garzon <asopapos@gmail.com>
2014-02-13 02:23:18 +0000

    Tests for the validator
    git-svn-id: http://umple.googlecode.com/svn/trunk@3468 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umple.validator/test/cruise/umple/validator/JavaCodeGenValidatorTest.java
@@ -0,0 +1,33 @@
+package cruise.umple.validator;
+
+import java.io.File;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import cruise.umple.compiler.UmpleModel;
+import cruise.umple.util.SampleFileWriter;
+
+public class JavaCodeGenValidatorTest {
+	  JavaCodeGenValidator validator ;
+	  UmpleModel model;
+	  String pathToInput;
+	  String pathToRoot ;
+
+	  @Before
+	  public void setUp()
+	  {
+		pathToInput = SampleFileWriter.rationalize("test/cruise/umple/validator");
+		pathToRoot = SampleFileWriter.rationalize("../cruise.umple.validator");
+	    model = new UmpleModel(null);
+	    validator = new JavaCodeGenValidator();
+	    
+	  }
+	  
+	@Test
+	public void UmpleFileWithMultipleClasses() {
+	    boolean result = validator.validateGeneratedCode(pathToInput + "/Valid_UmpleFileWithMultipleClasses.ump");
+	    Assert.assertTrue(result);
+	}
+}
