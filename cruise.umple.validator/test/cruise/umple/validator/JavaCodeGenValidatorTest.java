0da9151e85b78c1ea4bc0cf0d49a10b34ffff6cb
Miguel Garzon <asopapos@gmail.com>
2014-02-13 17:14:50 +0000

    More test cases - validator working well!
    git-svn-id: http://umple.googlecode.com/svn/trunk@3471 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple.validator/test/cruise/umple/validator/JavaCodeGenValidatorTest.java
+++ cruise.umple.validator/test/cruise/umple/validator/JavaCodeGenValidatorTest.java
@@ -1,30 +1,61 @@
 package cruise.umple.validator;
 
-import java.io.File;
+import junit.framework.Assert;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import cruise.umple.compiler.UmpleFile;
 import cruise.umple.compiler.UmpleModel;
 import cruise.umple.util.SampleFileWriter;
 
 public class JavaCodeGenValidatorTest {
-	  JavaCodeGenValidator validator ;
-	  UmpleModel model;
+	  JavaCodeGenValidator validator;
 	  String pathToInput;
-	  String pathToRoot ;
+	  String pathToRoot;
 
 	  @Before
 	  public void setUp()
 	  {
 		pathToInput = SampleFileWriter.rationalize("test/cruise/umple/validator");
 		pathToRoot = SampleFileWriter.rationalize("../cruise.umple.validator");
-	    model = new UmpleModel(null);
-	    validator = new JavaCodeGenValidator();
-	    
+	    validator = new JavaCodeGenValidator();  
+	  }
+	  
+	  @After
+	  public void tearDown()
+	  {
+	    SampleFileWriter.destroy(pathToInput + "/A.java");
+	    SampleFileWriter.destroy(pathToInput + "/C.java");
 	  }
 	  
 	@Test
 	public void UmpleFileWithMultipleClasses() {
-	    boolean result = validator.validateGeneratedCode(pathToInput + "/Valid_UmpleFileWithMultipleClasses.ump");
+		UmpleFile umpleFile =  new UmpleFile(pathToInput + "/Valid_UmpleFileWithMultipleClasses.ump");
+	    boolean result = validator.validateGeneratedCode(umpleFile);
 	    Assert.assertTrue(result);
 	}
+	@Test
+	public void UmpleFileWithMultipleClasses_OneInvalidUmpleClass() {
+		UmpleFile umpleFile =  new UmpleFile(pathToInput + "/Invalid_UmpleFileWithMultipleClasses_OneInvalidUmpleClass.ump");
+	    boolean result = validator.validateGeneratedCode(umpleFile);
+	    Assert.assertFalse(result);
+	}
+	
+	@Test
+	public void UmpleFileWithOneClass_OneValidUmpleClass() {
+		UmpleFile umpleFile =  new UmpleFile(pathToInput + "/Valid_UmpleFileWithOneClass_OneValidUmpleClass.ump");
+	    boolean result = validator.validateGeneratedCode(umpleFile);
+	    Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void UmpleFileWithOneClass_OneInvalidUmpleClass() {
+		UmpleFile umpleFile =  new UmpleFile(pathToInput + "/Invalid_UmpleFileWithOneClass_OneInvalidUmpleClass.ump");
+	    boolean result = validator.validateGeneratedCode(umpleFile);
+	    Assert.assertFalse(result);
+	}
+	
+	
 }
