fbe50d98c4c450d40d0011bffde306319a938744
Sultan Eid <thesultan.eid@gmail.com>
2014-02-20 21:41:01 +0000

    Setup for model-based test generator.
    git-svn-id: http://umple.googlecode.com/svn/trunk@3524 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umple/test/cruise/umple/implementation/mbtest/mbtestTemplateTest.java
@@ -0,0 +1,20 @@
+package cruise.umple.implementation.mbtest;
+
+import org.junit.Before;
+/*
+import cruise.umple.implementation.TemplateTest;
+
+
+
+public class mbtestTemplateTest extends TemplateTest{
+
+	  @Before
+	  public void setUp()
+	  {
+	super.setUp();
+    language = "mbtest";
+    languagePath = "mbtest";
+    
+	  }
+}
+*/
