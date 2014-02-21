8cfb3f8f6f771d0cef8c0564f8ff6e0cdab337cc
Miguel Garzon <asopapos@gmail.com>
2014-02-21 16:19:54 +0000

    umplificator core project
    git-svn-id: http://umple.googlecode.com/svn/trunk@3537 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umplificator/src/cruise/umplificator/core/AssociationRefactoringStep.java
@@ -0,0 +1,34 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.19.0.3287 modeling language!*/
+
+package cruise.umplificator.core;
+
+// line 20 "../../../Umplificator_core.ump"
+public class AssociationRefactoringStep extends RefactoringStep
+{
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+  public @interface umplesourcefile{int[] line();String[] file();int[] javaline();int[] length();}
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public AssociationRefactoringStep()
+  {
+    super();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
