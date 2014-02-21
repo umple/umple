09c6f5e135865ce94225e5b0e48db18c6f36dfb5
asopapos@gmail.com <asopapos@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-02-21 16:19:54 +0000

    umplificator core project
    git-svn-id: http://umple.googlecode.com/svn/trunk@3537 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umplificator/src/cruise/umplificator/core/InitialRefactoringStep.java
@@ -0,0 +1,34 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.19.0.3287 modeling language!*/
+
+package cruise.umplificator.core;
+
+// line 12 "../../../Umplificator_core.ump"
+public class InitialRefactoringStep extends RefactoringStep
+{
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+  public @interface umplesourcefile{int[] line();String[] file();int[] javaline();int[] length();}
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public InitialRefactoringStep()
+  {
+    super();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
---
+++ cruise.umplificator/src/cruise/umplificator/core/StateMachineRefactoringStep.java
@@ -0,0 +1,34 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.19.0.3287 modeling language!*/
+
+package cruise.umplificator.core;
+
+// line 24 "../../../Umplificator_core.ump"
+public class StateMachineRefactoringStep extends RefactoringStep
+{
+  @java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+  public @interface umplesourcefile{int[] line();String[] file();int[] javaline();int[] length();}
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public StateMachineRefactoringStep()
+  {
+    super();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {
+    super.delete();
+  }
+
+}
