d65728350ac2dd52ffa77934cb66ef9021a6c7ab
asopapos@gmail.com <asopapos@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-02-28 14:36:00 +0000

    Console main, FileHelper and Tests for console
    git-svn-id: http://umple.googlecode.com/svn/trunk@3574 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

---
+++ cruise.umplificator/test/cruise/umplificator/UmplificatorMainConsoleTest.java
@@ -0,0 +1,32 @@
+package cruise.umplificator;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import cruise.umplificator.UmplificatorMain;
+
+public class UmplificatorMainConsoleTest {
+
+	@Test
+	public void MainOneUmpleFileAsInput() {
+	String[] args = new String[] {"testclass.ump"};
+		
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter("testclass.ump"));
+		    out.write("class testclass {}");
+		    out.close();
+		    
+		    UmplificatorMain.main(args);
+		    
+		    new File("testclass.ump").delete();
+		} catch (IOException e) {
+			Assert.fail();
+		}
+	}
+
+}
