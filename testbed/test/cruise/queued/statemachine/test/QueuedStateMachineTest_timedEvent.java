4e7c5027d190e4c8c349660ad4db4d2706eb975b
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-04-07 12:37:19 +0000

    Commenting out test QueuedSM_timedEvent because it was nondeterministic and was causing sporadic build failure
    git-svn-id: http://umple.googlecode.com/svn/trunk@3869 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- testbed/test/cruise/queued/statemachine/test/QueuedStateMachineTest_timedEvent.java
+++ testbed/test/cruise/queued/statemachine/test/QueuedStateMachineTest_timedEvent.java
@@ -2,6 +2,7 @@ package cruise.queued.statemachine.test;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Ignore;
 
 
 public class QueuedStateMachineTest_timedEvent
@@ -9,44 +10,46 @@ public class QueuedStateMachineTest_timedEvent
   @Test 
   public void numberOfMessagesInMessageType()
   {
-	  // compare the number of messages in MessageType is equal to the number of events in State Machine except timed events and auto-transition
-	  Assert.assertEquals(0, QueuedSM_timedEvent.MessageType.values().length); 
+    // compare the number of messages in MessageType is equal to the number of events in State Machine except timed events and auto-transition
+    Assert.assertEquals(0, QueuedSM_timedEvent.MessageType.values().length); 
   }
   
-  @Test 
+  @Ignore @Test
+  // Test ignored for now as it is nondeterministic - the waiting times are
+  // not guaranteed. Test needs replacing
   public void processEvents() throws InterruptedException
   {
-	  QueuedSM_timedEvent qsm = new QueuedSM_timedEvent();
-	  // check initial state is s1
-	  Assert.assertEquals(QueuedSM_timedEvent.Status.s1, qsm.getStatus());
-	  Assert.assertEquals(3, qsm.getTimer());
-	  //waiting for 4 seconds before transition to s2
-	  wait(5);
-		  
-	  //transition to s2
-	  Assert.assertEquals(QueuedSM_timedEvent.Status.s2, qsm.getStatus());
-	  wait(4);
-		  
-	  Assert.assertEquals(QueuedSM_timedEvent.Status.s1, qsm.getStatus());
-	  wait(5);
-		  
-	  Assert.assertEquals(QueuedSM_timedEvent.Status.s2, qsm.getStatus());
-		  
-		  
-	  //check that there is no event left in the queue
-	  Assert.assertEquals(0, qsm.pool.messages.size());
-		  
+    QueuedSM_timedEvent qsm = new QueuedSM_timedEvent();
+    // check initial state is s1
+    Assert.assertEquals(QueuedSM_timedEvent.Status.s1, qsm.getStatus());
+    Assert.assertEquals(3, qsm.getTimer());
+    //waiting for 4 seconds before transition to s2
+    wait(5);
+      
+    //transition to s2
+    Assert.assertEquals(QueuedSM_timedEvent.Status.s2, qsm.getStatus());
+    wait(4);
+      
+    Assert.assertEquals(QueuedSM_timedEvent.Status.s1, qsm.getStatus());
+    wait(5);
+      
+    Assert.assertEquals(QueuedSM_timedEvent.Status.s2, qsm.getStatus());
+      
+      
+    //check that there is no event left in the queue
+    Assert.assertEquals(0, qsm.pool.messages.size());
+      
   }
   
   public static void wait(int seconds)
   {
-	  try
-	  {
-	    Thread.sleep(seconds*1000);
-	  }
-	  catch (InterruptedException e)
-	  {
-	    e.printStackTrace();
-	  }    
+    try
+    {
+      Thread.sleep(seconds*1000);
+    }
+    catch (InterruptedException e)
+    {
+      e.printStackTrace();
+    }    
   }
 }
