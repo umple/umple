934451f09a033b4543cd1b0c0a8206fc5060b875
Hamoud Aljamaan <hjamaan@gmail.com>
2014-04-09 06:28:55 +0000

    enhancing after opertaion code injection for "empty constructor" - php
    git-svn-id: http://umple.googlecode.com/svn/trunk@3880 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/implementation/java/JavaClassTemplateTest.java
+++ cruise.umple/test/cruise/umple/implementation/java/JavaClassTemplateTest.java
@@ -53,6 +53,11 @@ public class JavaClassTemplateTest extends ClassTemplateTest
   {
     assertUmpleTemplateFor("ClassTemplateTest_FixmlAttributes3.ump",languagePath + "/ClassTemplateTest_FixmlAttributes3."+ languagePath +".txt","Mentor");
   }
+  @Test @Ignore
+  public void FixmlAttributes4()
+  {
+    assertUmpleTemplateFor("ClassTemplateTest_FixmlAttributes4.ump",languagePath + "/ClassTemplateTest_FixmlAttributes4."+ languagePath +".txt","Mentor");
+  }
   
   //Ignored this test because it doesn't seem to be able to take a umple version like @UMPLE_VERSION@ instead it needs it to be an exact version
   @Test @Ignore
