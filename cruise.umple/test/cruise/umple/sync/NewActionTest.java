ec09444a10b55f464f49c7d94e41993ffc012a81
timothyclethbridge@gmail.com <timothyclethbridge@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2014-05-16 22:22:23 +0000

    Fixed issue 544 patch-CraigBryan-544-RemovingUmpleOnlineErrorCausedByUse
    git-svn-id: http://umple.googlecode.com/svn/trunk@4077 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/sync/NewActionTest.java
+++ cruise.umple/test/cruise/umple/sync/NewActionTest.java
@@ -11,16 +11,33 @@ package cruise.umple.sync;
 
 import org.junit.*;
 
+import cruise.umple.util.SampleFileWriter;
+
 public class NewActionTest
 {
+  
+  String pathToInput;
+  
+  @Before
+  public void setUp()
+  {
+    pathToInput = SampleFileWriter.rationalize("test/cruise/umple/sync");
+  }
+  
+  @After
+  public void tearDown() {
+    SampleFileWriter.destroy(pathToInput + "/myfile.ump");
+  }
   
   @Test
   public void Go_SameIdAsName()
   {
     String umple = "";
     String json = "{\"position\":{\"x\":1,\"y\":2,\"width\":3,\"height\":4},\"id\":\"Student\",\"name\":\"Student\"}";
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
     
-    NewAction action = new NewAction(json,umple);
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
     
     String expected = "class Student\n{\n}\n\nclass Student\n{\n  position 1 2 3 4;\n}\n";
@@ -31,39 +48,41 @@ public class NewActionTest
   public void Go_AddAfterLastClassInModel()
   {
     String umple = 
-    	    "class NewClass" +
-    		"\n{" +
-    		"\n}" +
-    		"\n" +
-    		"\nclass NewClass" +
-    		"\n{" +
-    		"\n  position 124 88 109 42;" +
-    		"\n}" +
-    		"\n";
+        "class NewClass" +
+        "\n{" +
+        "\n}" +
+        "\n" +
+        "\nclass NewClass" +
+        "\n{" +
+        "\n  position 124 88 109 42;" +
+        "\n}" +
+        "\n";
     String json = "{\"position\" : {\"x\" : \"130\",\"y\" : \"199\",\"width\" : \"109\",\"height\" : \"42\"},\"attributes\" : [],\"id\" : \"NewClass1\",\"name\" : \"NewClass1\"}";
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
     
-    NewAction action = new NewAction(json,umple);
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
     
     String expected = 
-    	    "class NewClass" +
-    		"\n{" +
-    		"\n}" +
-    		"\n" +
-    		"\nclass NewClass1" +
-    		"\n{" +
-    		"\n}" +
-    		"\n" +
-    		"\nclass NewClass" +
-    		"\n{" +
-    		"\n  position 124 88 109 42;" +
-    		"\n}" +
-    		"\n" +
-    		"\nclass NewClass1" +
-    		"\n{" +
-    		"\n  position 130 199 109 42;" +
-    		"\n}" +
-    		"\n";
+        "class NewClass" +
+        "\n{" +
+        "\n}" +
+        "\n" +
+        "\nclass NewClass1" +
+        "\n{" +
+        "\n}" +
+        "\n" +
+        "\nclass NewClass" +
+        "\n{" +
+        "\n  position 124 88 109 42;" +
+        "\n}" +
+        "\n" +
+        "\nclass NewClass1" +
+        "\n{" +
+        "\n  position 130 199 109 42;" +
+        "\n}" +
+        "\n";
     Assert.assertEquals(expected, action.getUmpleCode());
   }  
   
@@ -72,8 +91,10 @@ public class NewActionTest
   {
     String umple = "";
     String json = "{\"position\":{\"x\":1,\"y\":2,\"width\":3,\"height\":4},\"id\":\"umpleClass_1\",\"name\":\"Student\"}";
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
     
-    NewAction action = new NewAction(json,umple);
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
     
     String expected = "class Student\n{\n}\n\nclass Student\n{\n  position 1 2 3 4;\n}\n";
@@ -85,10 +106,12 @@ public class NewActionTest
   {
     String umple = "blah";
     String json = "{\"position\":{\"x\":1,\"y\":2,\"width\":3,\"height\":4},\"id\":\"umpleClass_1\",\"name\":\"Student\"}";
-
-    NewAction action = new NewAction(json,umple);
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
+    
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
-    System.out.println(action.getUmpleCode());
+    
     Assert.assertEquals("class Student\n{\n}\n\nblah\nclass Student\n{\n  position 1 2 3 4;\n}\n",action.getUmpleCode());
   }   
   
@@ -97,8 +120,10 @@ public class NewActionTest
   {
     String umple = "//$?[End_of_model]$?";
     String json = "{\"position\":{\"x\":1,\"y\":2,\"width\":3,\"height\":4},\"id\":\"umpleClass_1\",\"name\":\"Student\"}";
-
-    NewAction action = new NewAction(json,umple);
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
+    
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
     
     Assert.assertEquals("class Student\n{\n}\n\n//$?[End_of_model]$?\nclass Student\n{\n  position 1 2 3 4;\n}\n",action.getUmpleCode());
@@ -109,8 +134,10 @@ public class NewActionTest
   {
     String umple = "//This is a comment in the code\n//$?[End_of_model]$?";
     String json = "{\"position\":{\"x\":1,\"y\":2,\"width\":3,\"height\":4},\"id\":\"umpleClass_1\",\"name\":\"Student\"}";
-
-    NewAction action = new NewAction(json,umple);
+    String filename = pathToInput + "/myfile.ump";
+    SampleFileWriter.createFile(filename, umple);
+    
+    NewAction action = new NewAction(json,umple,filename);
     action.go();
     
     Assert.assertEquals("//This is a comment in the code\nclass Student\n{\n}\n\n//$?[End_of_model]$?\nclass Student\n{\n  position 1 2 3 4;\n}\n",action.getUmpleCode());
