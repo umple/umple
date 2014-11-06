29df9d48d117d7d34e5b7c8082f0529e45d39858
Opeyemi Adesina <opeyemi.adesina@gmail.com>
2014-11-06 21:51:07 +0000

    git-svn-id: http://umple.googlecode.com/svn/trunk@4720 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/src-gen-jet/cruise/umple/compiler/java/JavaClassGenerator.java
+++ cruise.umple/src-gen-jet/cruise/umple/compiler/java/JavaClassGenerator.java
@@ -18,7 +18,7 @@ public class JavaClassGenerator implements ILang
   }
 
   public final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
-  protected final String TEXT_1 = "/*PLEASE DO NOT EDIT THIS CODE*/" + NL + "/*This code was generated using the UMPLE ${last.version} modeling language!*/";
+  protected final String TEXT_1 = "/*PLEASE DO NOT EDIT THIS CODE*/" + NL + "/*This code was generated using the UMPLE 1.21.0.4666 modeling language!*/";
   protected final String TEXT_2 = NL;
   protected final String TEXT_3 = NL;
   protected final String TEXT_4 = NL;
