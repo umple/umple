f090433997a47bf9afd5c06c25b4e69b060543fb
aforward@gmail.com <aforward@gmail.com@6ebf9d09-8eb1-0036-9d08-68c07d1b990a>
2015-02-22 02:28:17 +0000

    [639] cleaned up scxml output and added actions (by Warren Marivel)
    git-svn-id: http://umple.googlecode.com/svn/trunk@5137 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/implementation/scxml/ScxmlTemplateTest.java
+++ cruise.umple/test/cruise/umple/implementation/scxml/ScxmlTemplateTest.java
@@ -35,6 +35,9 @@ public class ScxmlTemplateTest extends TemplateTest
     SampleFileWriter.destroy(pathToInput + "/scxml/oneState.scxml");
     SampleFileWriter.destroy(pathToInput + "/scxml/simpleTransition.scxml");
     SampleFileWriter.destroy(pathToInput + "/scxml/nestedStateMachine.scxml");
+    SampleFileWriter.destroy(pathToInput + "/scxml/transitionWithGuard.scxml");
+    SampleFileWriter.destroy(pathToInput + "/scxml/onEntry.scxml");
+    SampleFileWriter.destroy(pathToInput + "/scxml/transitionAction.scxml");
   }
 
   @Test
@@ -59,5 +62,23 @@ public class ScxmlTemplateTest extends TemplateTest
   public void nestedStateMachine()
   {
     assertUmpleTemplateFor("scxml/nestedStateMachine.ump","scxml/nestedStateMachine.scxml.txt");
+  }
+  
+  @Test
+  public void transitionWithGuard()
+  {
+    assertUmpleTemplateFor("scxml/transitionWithGuard.ump","scxml/transitionWithGuard.scxml.txt");
+  }
+  
+  @Test
+  public void stateWithEntry()
+  {
+    assertUmpleTemplateFor("scxml/onEntry.ump","scxml/onEntry.scxml.txt");
+  }
+  
+  @Test
+  public void transitionAction()
+  {
+    assertUmpleTemplateFor("scxml/transitionAction.ump","scxml/transitionAction.scxml.txt");
   }
 }
