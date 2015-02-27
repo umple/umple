70ac812c16468d6c96bc3293aaf9bdb5f5c79236
Timothy Lethbridge <timothyclethbridge@gmail.com>
2015-02-27 03:23:39 +0000

    Updating travis config
    git-svn-id: http://umple.googlecode.com/svn/trunk@5162 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- travis/deps.linux.sh
+++ travis/deps.linux.sh
@@ -6,6 +6,7 @@ set -e
 # check for linux
 test "$TRAVIS_OS_NAME" == "linux"
 
-sudo apt-get install php5-gd php-pear php-apc php5-curl libgd2-xpm libt1-5 php5-cli php5-common php5-dev shtool
+sudo apt-get update
+sudo apt-get install --force-yes php5-cli 
 
 sudo ln -s /usr/share/java/ant-contrib.jar /usr/share/ant/lib/
