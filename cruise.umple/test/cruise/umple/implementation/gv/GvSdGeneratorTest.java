0090562263c330281d690facad03398afeb255ae
Craig Bryan <craigbryan.csb@gmail.com>
2015-03-02 15:20:30 +0000

    Fixed issue 679. Also fixed code formatting in cruise.umple/src/Generator_CodeGvStateDiagram.ump.
    git-svn-id: http://umple.googlecode.com/svn/trunk@5170 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/implementation/gv/GvSdGeneratorTest.java
+++ cruise.umple/test/cruise/umple/implementation/gv/GvSdGeneratorTest.java
@@ -44,152 +44,184 @@ public class GvSdGeneratorTest extends TemplateTest {
     SampleFileWriter.destroy(pathToInput + "/gv/HideActionTransition.gv");
     SampleFileWriter.destroy(pathToInput + "/gv/HideGuardTransition.gv");
     SampleFileWriter.destroy(pathToInput + "/gv/AutoTransition.gv");
+    SampleFileWriter.destroy(pathToInput + "/gv/GuardedAutoTransition.gv");
     SampleFileWriter.destroy(pathToInput + "/gv/TimedTransition.gv");
     SampleFileWriter.destroy(pathToInput + "/gv/ParameterTransition.gv");
   }
 
   /********* STATE TESTS **********/
   @Test
-  public void tooltip_normal_state() {
+  public void tooltip_normal_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/NormalState.ump","gv/NormalState.gv.txt");
   }
 
   @Test
-  public void tooltip_nested_state() {
+  public void tooltip_nested_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/NestedState.ump","gv/NestedState.gv.txt");
   }
 	
   @Test
-  public void tooltip_entry_less5_state() {
+  public void tooltip_entry_less5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/EntryLess5State.ump","gv/EntryLess5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_entry_5_state() {
+  public void tooltip_entry_5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/Entry5State.ump","gv/Entry5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_entry_more5_state() {
+  public void tooltip_entry_more5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/EntryMore5State.ump","gv/EntryMore5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_exit_less5_state() {
+  public void tooltip_exit_less5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ExitLess5State.ump","gv/ExitLess5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_exit_5_state() {
+  public void tooltip_exit_5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/Exit5State.ump","gv/Exit5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_exit_more5_state() {
+  public void tooltip_exit_more5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ExitMore5State.ump","gv/ExitMore5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_less5_state() {
+  public void tooltip_activity_less5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ActivityLess5State.ump","gv/ActivityLess5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_5_state() {
+  public void tooltip_activity_5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/Activity5State.ump","gv/Activity5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_more5_state() {
+  public void tooltip_activity_more5_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ActivityMore5State.ump","gv/ActivityMore5State.gv.txt");
   }
 	
   @Test
-  public void tooltip_hide_action_state() {
+  public void tooltip_hide_action_state()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/HideActionState.ump","gv/HideActionState.gv.txt");
   }
 	
   /********* TRANSITION TESTS **********/
   @Test
-  public void tooltip_transition() {
+  public void tooltip_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/Transition.ump","gv/Transition.gv.txt");
   }
     
   @Test
-  public void tooltip_transition_self() {
+  public void tooltip_transition_self()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/TransitionSelf.ump","gv/TransitionSelf.gv.txt");
   }
 	
   @Test
-  public void tooltip_nested_transition() {
+  public void tooltip_nested_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/NestedTransition.ump","gv/NestedTransition.gv.txt");
   }
 	
   @Test
-  public void tooltip_guard_transition() {
+  public void tooltip_guard_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/GuardTransition.ump","gv/GuardTransition.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_less5_transition() {
+  public void tooltip_activity_less5_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ActLess5Transition.ump","gv/ActLess5Transition.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_5_transition() {
+  public void tooltip_activity_5_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/Act5Transition.ump","gv/Act5Transition.gv.txt");
   }
 	
   @Test
-  public void tooltip_activity_more5_transition() {
+  public void tooltip_activity_more5_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ActMore5Transition.ump","gv/ActMore5Transition.gv.txt");
   }
 	
   @Test
-  public void tooltip_hide_action_transition() {
+  public void tooltip_hide_action_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/HideActionTransition.ump","gv/HideActionTransition.gv.txt");
   }
 
   @Test
-  public void tooltip_hide_guard_transition() {
+  public void tooltip_hide_guard_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/HideGuardTransition.ump","gv/HideGuardTransition.gv.txt");
   }
 	
   @Test
-  public void tooltip_auto_transition() {
+  public void tooltip_auto_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/AutoTransition.ump","gv/AutoTransition.gv.txt");
   }
+
+  @Test
+  public void tooltip_guarded_auto_transition()
+  {
+    language = null;
+    assertUmplePartialTemplateFor("gv/GuardedAutoTransition.ump","gv/GuardedAutoTransition.gv.txt");
+  }
 	
   @Test
-  public void tooltip_timed_transition() {
+  public void tooltip_timed_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/TimedTransition.ump","gv/TimedTransition.gv.txt");
   }
 
   @Test
-  public void tooltip_parameter_transition() {
+  public void tooltip_parameter_transition()
+  {
     language = null;
     assertUmplePartialTemplateFor("gv/ParameterTransition.ump","gv/ParameterTransition.gv.txt");
   }
