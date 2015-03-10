3235e1b5c2970972212cf83582d79d7b7742b92b
Andrew Forward <aforward@gmail.com>
2015-03-10 11:23:56 +0000

    [681] ensuring that empty filter lets all data pass through (by Vivian Kou)
    git-svn-id: http://umple.googlecode.com/svn/trunk@5200 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umple/test/cruise/umple/compiler/FilterTest.java
+++ cruise.umple/test/cruise/umple/compiler/FilterTest.java
@@ -46,7 +46,7 @@ public class FilterTest
   {
     assertEquals(null, model.getFilter());
   }
-  
+
   @Test
   public void getFilter_defaultsToNull()
   {
@@ -58,7 +58,6 @@ public class FilterTest
   {
     Filter f = new Filter("aaa");
     model.setFilter(f);
-
     model.getFilter().addValue("Student");
     model.getFilter().addValue("Mentor");
     assertEquals(f, model.getFilter());
@@ -84,7 +83,30 @@ public class FilterTest
     Assert.assertEquals(false, f.isIncluded((UmpleClass)null));
   }
 
-  //@Test
+  @Test
+  public void isEmpty_hasNothing()
+  {
+    Filter f = new Filter("bbb");
+    Assert.assertEquals(true, f.isEmpty());
+  }
+
+  @Test
+  public void isEmpty_handlesNull()
+  {
+    Filter f = new Filter("bbb");
+    f.addValue(null);
+    Assert.assertEquals(false, f.isEmpty());
+  }
+
+  @Test
+  public void isEmpty_hasValues()
+  {
+    Filter f = new Filter("bbb");
+    f.addValue("abc");
+    Assert.assertEquals(false, f.isEmpty());
+  }
+
+  @Test
   public void applyFilter_OnlyKeepListedClasses()
   {
     UmpleModel model = parse("601_simpleFilter.ump");
@@ -95,10 +117,40 @@ public class FilterTest
   @Test
   public void applyFilter_Association()
   {
-	  UmpleModel model = parse("602_associationFilter.ump");
-	  model.applyFilter();
-	  Assert.assertEquals(1, model.getAssociations().size());
+    UmpleModel model = parse("602_associationFilter.ump");
+    model.applyFilter();
+    Assert.assertEquals(1, model.getAssociations().size());
   }
+
+  @Test
+  public void applyFilter_Empty()
+  {
+    UmpleModel model = parse("603_defaultFilter.ump");
+    model.applyFilter();
+    Assert.assertEquals(3, model.numberOfUmpleClasses());
+  }
+
+  @Test
+  public void applyFilter_IncludeAll()
+  {
+    UmpleModel model = parse("603_includeAllFilter.ump");
+    model.applyFilter();
+    Assert.assertEquals(3, model.numberOfUmpleClasses());
+  }
+
+
+  @Test
+  public void applyFilter_NullCheck()
+  {
+    Filter f = new Filter("aaa");
+    model.addUmpleClass("Mentor");
+    model.addUmpleClass("Student");
+    model.setFilter(f);
+    model.getFilter().addValue(null);
+    model.applyFilter();
+    Assert.assertEquals(0, model.numberOfUmpleClasses());
+  }
+
   public UmpleModel parse(String filename)
   {
     //String input = SampleFileWriter.readContent(new File(pathToInput, filename));
