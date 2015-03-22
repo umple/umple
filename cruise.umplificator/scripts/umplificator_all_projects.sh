e2f7b1232d3d35e7a8c33e47637b27b64b9ee92a
Miguel Garzon <asopapos@gmail.com>
2015-03-22 17:56:33 +0000

    [umplificator] Umplificator Logging Changes.    patch-zwiep-687-UmplificatorLoggingChanges
    git-svn-id: http://umple.googlecode.com/svn/trunk@5251 6ebf9d09-8eb1-0036-9d08-68c07d1b990a

--- cruise.umplificator/scripts/umplificator_all_projects.sh
+++ cruise.umplificator/scripts/umplificator_all_projects.sh
@@ -37,32 +37,36 @@ done
 for dir in "${to_umplify[@]}"; do
 
         touch $dir/P.umplify.score
+        echo $dir/P.umplify.score
 
         # Try level 0
-        java -jar $umplificator -level=0 -dir -path=$dir/umple_output $dir
+        java -jar $umplificator -level=0 -dir -path=$dir/umple_output $dir 2> ${umplificator%/*}/logs/stderr-level0.log
         if [ $? -eq 0 ]; then 
                 touch $dir/0.umplify.score
         else 
                 touch $dir/F.umplify.score
                 rm $dir/P.umplify.score
+                cp ${umplificator%/*}/logs/* $dir/logs/
                 continue
         fi
 
         # Try level 1
-        java -jar $umplificator -level=1 -dir -path=$dir/umple_output $dir 
+        java -jar $umplificator -level=1 -dir -path=$dir/umple_output $dir 2> ${umplificator%/*}/logs/stderr-level1.log
         if [ $? -eq 0 ]; then 
                 touch $dir/1.umplify.score
         else 
                 rm $dir/P.umplify.score
+                cp ${umplificator%/*}/logs/* $dir/logs/
                 continue
         fi
 
         # Try level 2
-        java -jar $umplificator -level=2 -dir -path=$dir/umple_output $dir
+        java -jar $umplificator -level=2 -dir -path=$dir/umple_output $dir 2> ${umplificator%/*}/logs/stderr-level2.log
         if [ $? -eq 0 ]; then 
                 touch $dir/2.umplify.score
         else 
                 rm $dir/P.umplify.score
+                cp ${umplificator%/*}/logs/* $dir/logs/
                 continue
         fi
 done
