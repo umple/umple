2e57cbf2c39885422b8d0cda1d2368b7a01ffb14
Kevin Brightwell <kevin.brightwell2@gmail.com>
2015-08-23 22:17:50 -0400

    Upgrade simpletest version, pull it directly from sourceforge

--- testbed_php/test/AllTests.php
+++ testbed_php/test/AllTests.php
@@ -1,11 +1,11 @@
 <?php
 date_default_timezone_set("America/New_York");
 
-require_once('../../lib/simpletest/unit_tester.php');
-require_once('../../lib/simpletest/reporter.php');
+require_once('../../dist/libs/php/simpletest/unit_tester.php');
+require_once('../../dist/libs/php/simpletest/reporter.php');
 require_once('AllTestHelper.php');
 
-$test = &new GroupTest('All Tests');
+$test = new TestSuite('All Tests');
 loadTestsIn($test,'associations');
 loadTestsIn($test,'statemachine');
 loadTestsIn($test,'attributes');
