29de05bc3af4f6915111276768f7fe9fb578c5f2
Timothy Lethbridge <timothyclethbridge@gmail.com>
2016-03-22 01:00:21 -0400

    Release 1.24.0 - final move from Jet to UmpleTL

--- cruise.umple/test/cruise/umple/implementation/AssociationBothSidesSortedTest.java
+++ cruise.umple/test/cruise/umple/implementation/AssociationBothSidesSortedTest.java
@@ -4,12 +4,12 @@ import org.junit.*;
 
 public class AssociationBothSidesSortedTest extends TemplateTest {
 	
-	@Test @Ignore
+	@Test
 	public void AssociationBothSidesSorted1()
 	{
 		assertUmpleTemplateFor("AssociationBothSidesSorted.ump", languagePath + "/AssociationBothSidesSorted_Mentor."+ languagePath +".txt", "Mentor");
 	}
-	@Test @Ignore
+	@Test
 	public void AssociationBothSidesSorted2()
 	{
 		assertUmpleTemplateFor("AssociationBothSidesSorted.ump", languagePath + "/AssociationBothSidesSorted_Student."+ languagePath +".txt", "Student");
