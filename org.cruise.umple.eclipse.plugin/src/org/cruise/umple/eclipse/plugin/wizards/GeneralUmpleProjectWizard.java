03e5f14ec40dbea073b2ffb0ae9dd2a17d7372ab
Vahdat Abdelzad <v.abdelzad@gmail.com>
2016-07-04 19:03:54 -0400

    This will allow to create an Umple project (file) through Eclipse normal process.

--- org.cruise.umple.eclipse.plugin/src/org/cruise/umple/eclipse/plugin/wizards/GeneralUmpleProjectWizard.java
+++ org.cruise.umple.eclipse.plugin/src/org/cruise/umple/eclipse/plugin/wizards/GeneralUmpleProjectWizard.java
@@ -1,10 +1,16 @@
 package org.cruise.umple.eclipse.plugin.wizards;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
-import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.*;
 import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+import org.eclipse.ui.ide.IDE;
 
 public class GeneralUmpleProjectWizard extends Wizard implements INewWizard {
 	
@@ -32,7 +38,30 @@ public class GeneralUmpleProjectWizard extends Wizard implements INewWizard {
 
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IWorkspaceRoot root = workspace.getRoot();
+		IProject project  = root.getProject(fPage.getProjectName());
+//		IFolder folder = project.getFolder("src-umple");
+		IFile file = project.getFile("main.ump");		
+			
+		try {
+			//at this point, no resources have been created	
+			if (!project.exists()) project.create(null);
+			if (!project.isOpen()) project.open(null);
+//			if (!folder.exists()) folder.create(IResource.NONE, true, null);
+			if (!file.exists()) {
+				//String content ="class Main{"+System.lineSeparator()+System.lineSeparator()+"}";
+				String content ="";
+			    byte[] bytes = content.getBytes();
+			    InputStream source = new ByteArrayInputStream(bytes);
+			    file.create(source, IResource.NONE, null);
+			}
+			//Open the filer in the editor
+			IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+			IDE.openEditor(page, file);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}	
 		return true;
 	}
 
