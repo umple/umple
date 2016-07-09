36f11ee64e2be1639be96f011e1fcc5846c93920
Vahdat Abdelzad <v.abdelzad@gmail.com>
2016-07-08 22:34:32 -0400

    a simple text editor with icon for ump files were added. There is no text highlighter in this version.

---
+++ org.cruise.umple.eclipse.plugin/src/org/cruise/umple/eclipse/plugin/editors/UMPEditor.java
@@ -0,0 +1,11 @@
+package org.cruise.umple.eclipse.plugin.editors;
+
+import org.eclipse.ui.editors.text.TextEditor;
+
+public class UMPEditor extends TextEditor {
+
+	public UMPEditor() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
