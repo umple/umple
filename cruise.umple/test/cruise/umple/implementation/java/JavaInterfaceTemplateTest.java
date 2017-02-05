c1b86996652d57684b02df560b7efb88beb0438e
Adam Bolding-Jones <anb1@sfu.ca>
2017-02-05 13:35:32 -0800

    -Fixed recursive inheritance problem for interfaces (issue 992). -Added tests to verify correct 992 fix

--- cruise.umple/test/cruise/umple/implementation/java/JavaInterfaceTemplateTest.java
+++ cruise.umple/test/cruise/umple/implementation/java/JavaInterfaceTemplateTest.java
@@ -27,9 +27,16 @@ public class JavaInterfaceTemplateTest extends InterfaceTemplateTest
 	{
 		assertUmpleTemplateFor("InterfaceTemplateTest.ump",languagePath + "/InterfaceTemplateTest_singleImplements."+ languagePath +".txt","IFirstChild");
 	}
+	
 	@Test
 	public void isAMultipleInterface()
 	{
 		assertUmpleTemplateFor("InterfaceTemplateTest.ump",languagePath + "/InterfaceTemplateTest_multipleImplements."+ languagePath +".txt","ISecondChild");
+	}
+	
+	@Test
+	public void implementsMultipleInterface()
+	{
+		assertUmpleTemplateFor("InterfaceTemplateTest.ump",languagePath + "/InterfaceTemplateTest_implementsMultipleInterface."+ languagePath +".txt","A");
 	}
 }
