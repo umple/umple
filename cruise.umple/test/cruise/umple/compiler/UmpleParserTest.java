07216098ed6146482ea350989a84ee010243b314
Abdulaziz <gublan24@gmail.com>
2018-06-05 11:31:32 -0400

    a test case for the mixset use statement

--- cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
+++ cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
@@ -3024,7 +3024,16 @@ public class UmpleParserTest
 	  
 
   }
-  
+
+  @Test
+  public void mixsetUseStatement()
+  {
+	  assertNoWarningsParse("/mixset/mixsetUse.ump");
+	  
+
+  }
+    
+
   public boolean parse(String filename)
   {
     //String input = SampleFileWriter.readContent(new File(pathToInput, filename));
