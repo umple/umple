1af16041a6706c2046db2aead12a221dcaea1e7f
Ivan Ivan <yirans@sfu.ca>
2018-09-22 12:41:10 -0700

    v1

---
+++ cruise.umple/C.java
@@ -0,0 +1,28 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE @UMPLE_VERSION@ modeling language!*/
+
+
+
+// line 1 "umpleClassTest.ump"
+public class C
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public C()
+  {}
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {}
+
+}
