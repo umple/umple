c10b756c3bc36b148d08761318a401cd2d6ce0e5
Ivan Ivan <yirans@sfu.ca>
2018-09-22 11:39:47 -0700

    make comparison of two primitives with ==

---
+++ cruise.umple/B.java
@@ -0,0 +1,28 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE ${last.version} modeling language!*/
+
+
+
+// line 1 "umpleClassTest.ump"
+public class B
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public B()
+  {}
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {}
+
+}
