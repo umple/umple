71ef918c00ef4b23aef1e6635981c32d7e4b55cf
Timothy Lethbridge <timothyclethbridge@gmail.com>
2018-10-05 16:53:28 -0400

    Fixing RuntimeErrorUtil for Java 11 and Fixes #1176

--- testbed/test/cruise/runtime/RuntimeErrorUtil.java
+++ testbed/test/cruise/runtime/RuntimeErrorUtil.java
@@ -14,8 +14,8 @@ import java.io.BufferedReader;
 public class RuntimeErrorUtil
 {
 	static String UMPLE_ROOT = "../";
-	static String SOURCE_FOLDER = (UMPLE_ROOT + "testbed/bin/").replaceAll("/", File.separator);
-	static String EXPECTED_FOLDER = (UMPLE_ROOT + "testbed/test/cruise/runtime/expected/").replaceAll("/", File.separator);
+	static String SOURCE_FOLDER = (UMPLE_ROOT + "testbed/bin/").replace("/", File.separator);
+	static String EXPECTED_FOLDER = (UMPLE_ROOT + "testbed/test/cruise/runtime/expected/").replace("/", File.separator);
 	static String PACKAGE_NAME = "cruise.runtime.";
 
 	public static void AssertRuntimeError(String javaClass, String expectedOutputFile) {
@@ -44,6 +44,12 @@ public class RuntimeErrorUtil
 		String expected = expectedReader.readLine();
 		int line = 0;
 		while (error != null && expected != null) {
+		  // The following is a kludge to make certain tests pass in Java 11, where
+		  // extra information is included with the out of bounds message
+		  // This should be deleted and the tests InMain, IsATrait and BeforeAfter
+		  // should be upddated with extra output after Java 11 has been in use for a year
+		  // e.g. sometime in late 2019 or early 2020
+		  error = error.replace("ArrayIndexOutOfBoundsException: Index 1 out of bounds for length 1","ArrayIndexOutOfBoundsException: 1");
 			line++;
 			Assert.assertEquals("Failed at: " + line, expected.trim(), error.trim());
 			error = actualReader.readLine();
