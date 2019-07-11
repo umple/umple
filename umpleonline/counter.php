46b465dea7a0c940ef5bfc94f4959dc09061cbf5
fjtheknight <fj.the.knight@gmail.com>
2019-07-11 01:08:09 -0400

    show methods ever run on umpleonline and log.php

--- umpleonline/counter.php
+++ umpleonline/counter.php
@@ -9,6 +9,7 @@ http://justintadlock.com/web-design/counter
 //opens countlog.txt to read the number of hits
 $dir = dirname(__FILE__);
 $logpath = $dir."/countlog.txt";
+$commandlogpath = $dir."/scripts/commandcount.txt";
 if(file_exists($logpath)){
 	if(!is_writable($logpath)||!is_readable($logpath)) {
 		chmod($logpath, 0755);
@@ -24,6 +25,16 @@ if(file_exists($logpath)){
 	$log = fopen($logpath,"w");
 	fwrite($log, $count);
 	fclose($log);
+}
+
+if(file_exists($commandlogpath)){
+	if(!is_writable($commandlogpath)||!is_readable($commandlogpath)) {
+		chmod($commandlogpath, 0755);
+	}
+	$log = fopen($commandlogpath, "r");
+	$count = fgets($log,1000);
+	fclose($log);
+	echo " | $count commands run since July 2019" ;
 }
 
 
