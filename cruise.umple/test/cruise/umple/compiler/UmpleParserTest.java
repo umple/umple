d5b2b4645b82b9dfaaad68dbe5da5ce77b4ce7f9
fjtheknight <fj.the.knight@gmail.com>
2019-08-26 22:19:36 -0400

    fixes #1315 : added tests for warnings

--- cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
+++ cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
@@ -721,6 +721,31 @@ public class UmpleParserTest
   }
 
   @Test
+  public void redefineNamespace()
+  {
+    assertHasWarningsParse("002_redefineNamespace.ump",30,0);
+    assertHasWarningsParse("002_redefineNamespace.ump",30,1);
+    assertNoWarningsParse("002_defaultRedefineNamespace.ump");
+  }
+
+  @Test
+  public void notUsedNamespace()
+  {
+    assertHasWarningsParse("002_notUsedNamespace.ump", new Position("002_notUsedNamespace.ump",1,33,33),31);
+    assertHasWarningsParse("002_notUsedNamespace2.ump", new Position("002_notUsedNamespace2.ump",1,10,10),31);
+    assertHasWarningsParse("002_notUsedNamespace3.ump", new Position("002_notUsedNamespace3.ump",1,10,10),31);
+    assertHasWarningsParse("002_notUsedNamespace4.ump", new Position("002_notUsedNamespace4.ump",1,34,34),31);
+  }
+
+  @Test
+  public void noDefaultNamespace()
+  {
+    assertHasWarningsParse("002_noDefaultNamespace.ump",42,0);
+    assertHasWarningsParse("002_noDefaultNamespace.ump",42,1);
+    assertHasWarningsParse("002_noDefaultNamespace.ump",42,2);
+  }
+
+  @Test
   public void emptyClass()
   {
     assertParse("003_emptyClass.ump");
