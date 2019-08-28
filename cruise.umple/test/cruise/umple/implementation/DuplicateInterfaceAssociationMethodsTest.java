83e0c71d8b7e3ead1562673a34b5ebeaac657589
fjtheknight <fj.the.knight@gmail.com>
2019-08-28 12:44:36 -0400

    testing tests for 834

---
+++ cruise.umple/test/cruise/umple/implementation/DuplicateInterfaceAssociationMethodsTest.java
@@ -0,0 +1,24 @@
+package cruise.umple.implementation;
+
+import org.junit.*;
+
+public class DuplicateInterfaceAssociationMethodsTest extends TemplateTest {
+	
+	@Test
+	public void DuplicateInterfaceAssociationMethods()
+	{
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest1.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest1."+ languagePath +".txt", "First");
+		/*assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest2.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest2."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest3.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest3."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest4.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest4."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest5.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest5."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest6.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest6."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest7.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest7."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest8.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest8."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest9.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest9."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest10.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest10."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest11.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest11."+ languagePath +".txt", "First");
+		assertUmpleTemplateFirstor("DuplicateInterfaceAssociationMethodsTest12.ump", languagePath + "/DuplicateInterfaceAssociationMethodsTest12."+ languagePath +".txt", "First");*/
+	}
+	
+}
