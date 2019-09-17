4b2d409f66485218714cd858b19e50102911470a
Abdulaziz <gublan24@gmail.com>
2019-09-17 18:36:24 -0400

    destory generated files after test.

--- cruise.umple/test/cruise/umple/implementation/java/innerClass/InnerClassTest.java
+++ cruise.umple/test/cruise/umple/implementation/java/innerClass/InnerClassTest.java
@@ -2,25 +2,48 @@ package cruise.umple.implementation.java.innerClass;
 
 import org.junit.*;
 import cruise.umple.implementation.TemplateTest;
+import cruise.umple.util.SampleFileWriter;
 
 public class InnerClassTest extends TemplateTest {
+
+  @Before
+  public void setUp()
+  {
+    pathToInput = SampleFileWriter.rationalize("test/cruise/umple/implementation/java/innerClass");
+    pathToRoot = SampleFileWriter.rationalize("../cruise.umple");
+    language = null;
+    suboptions = null;
+    languagePath = "java";
+    umpleParserName = "cruise.umple.compiler.UmpleInternalParser";
+    aTracer = null;
+    tracerPath = null;
+  }
 	
 	@Test
 	public void TestInnerStaticClass()
-	{
-		assertUmpleTemplateFor("/java/innerClass/innerStatic.ump", languagePath + "/innerClass/innerStatic.java.txt", "OuterClass");
-	}
+  {
+    assertUmpleTemplateFor("/innerStatic.ump", "/innerStatic.java.txt", "OuterClass_1");
+  }
 
-	@Test
-	public void TestInnerNonStaticClass()
-	{
-		assertUmpleTemplateFor("/java/innerClass/innerNonStatic.ump", languagePath + "/innerClass/InnerNotStatic.java.txt", "OuterClass");
-	}
+  @Test
+  public void TestInnerNonStaticClass()
+  {
+    assertUmpleTemplateFor("/innerNonStatic.ump", "/InnerNotStatic.java.txt", "OuterClass_2");
+  }
+  @Test
+  public void TestInnerStaticAndNonStaticClass()
+  {
+    assertUmpleTemplateFor("/innerClasses.ump",  "/innerClasses.java.txt", "OuterClass_3");
+  }
 
-	public void TestInnerStaticAndNonStaticClass()
-	{
-		assertUmpleTemplateFor("/java/innerClass/innerClasses.ump", languagePath + "/innerClass/innerClasses.java.txt", "OuterClass");
-	}
+  @After
+  public void tearDown()
+  {
+    SampleFileWriter.destroy(pathToInput + "/OuterClass_1.java");
+    SampleFileWriter.destroy(pathToInput + "/OuterClass_2.java");
+    SampleFileWriter.destroy(pathToInput + "/OuterClass_3.java");
+  }
+
 }
 
 
