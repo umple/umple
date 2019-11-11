51c38723cf9bab12e47943b103076e01bc7cbaa5
Abdulaziz <gublan24@gmail.com>
2019-11-11 16:07:29 -0500

    fix missing package

--- cruise.umple/test/cruise/umple/compiler/mixset/UmpleMixsetTest.java
+++ cruise.umple/test/cruise/umple/compiler/mixset/UmpleMixsetTest.java
@@ -14,6 +14,7 @@ import cruise.umple.compiler.FeatureLink;
 import cruise.umple.compiler.FeatureNode;
 import cruise.umple.compiler.FeatureLeaf;
 import cruise.umple.compiler.Method;
+import cruise.umple.compiler.MethodBody;
 import cruise.umple.compiler.exceptions.*;
 import java.io.File;
 
