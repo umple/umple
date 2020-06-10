d7091d90ee09995e0687df04c370eda83d1c5e12
Abdulaziz <gublan24@gmail.com>
2020-06-10 15:08:29 -0400

    update js files

--- umpleonline/scripts/umple_action.js
+++ umpleonline/scripts/umple_action.js
@@ -1060,7 +1060,7 @@ Action.loadExample = function loadExample()
   }
  else if(Page.useGvFeatureDiagram) {
     diagramType="&diagramtype=GvFeature";
-    jQuery("#gencpp").prop("selected",true);
+    jQuery("#genjava").prop("selected",true);
   }
   else if(Page.useStructureDiagram) {
     diagramType="&diagramtype=structure&generateDefault=cpp";
