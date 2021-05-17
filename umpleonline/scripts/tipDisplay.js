ffd884c8576f768ea7f24d5c864704dd413c3800
Svetlana <svetlana2015e@gmail.com>
2021-05-17 17:11:03 -0400

    Made asynchronous, increased loading speed

--- umpleonline/scripts/tipDisplay.js
+++ umpleonline/scripts/tipDisplay.js
@@ -1,103 +1,97 @@
-alert("accessed file");
-
 var existCookie = document.cookie.match(/^(.*;)?\s*tipCookie\s*=\s*[^;]+(.*)?$/);
 var htmlNames = ["Importanttips.html","Secondarytips.html","Tertiarytips.html"];
 var fileNames = ["https://raw.githubusercontent.com/umple/umple/Issue%231636_TOTD/build/reference/6600ImportantTips.txt","https://raw.githubusercontent.com/umple/umple/Issue%231636_TOTD/build/reference/6601SecondaryTips.txt", "https://raw.githubusercontent.com/umple/umple/Issue%231636_TOTD/build/reference/6602TertiaryTips.txt"];
-
 
 if (existCookie==null){
     // =========================================
     // Tip Information Gatherers
     // =========================================
-
-    var firstTime = localStorage.getItem("first_time");
-    if(!firstTime) {
-        localStorage.setItem('tipCount',0);
-        localStorage.setItem('priorityCount',0);
-        
-        //acquiring tipInformation
-        for (let files=0; files<fileNames.length; files++){
-            jQuery.ajax({
-                url: fileNames[files],
-                async: false,
-                success: function(results){
-                    let allInfo=results;
-                    allInfo=allInfo.split("<h2>");
-                    if (allInfo!=null){
-                        alert("got info");
-                    }
-                    var descrpt=[];
-                    for (let i=0; i<allInfo.length; i++){
-                        var t=[];
-                        t.push(allInfo[i].substring(0, allInfo[i].indexOf("</h2>")));
-                        var link="http://manual.umple.org/manual/"+htmlNames[files];
-                        t.push(allInfo[i].substring(allInfo[i].indexOf("</h2>")+6, allInfo[i].length-1)+'<span style="float: right; padding-right: 10px; display:block;"><a href='+link+' style="color:#4d4d4d; text-align:right; text-decoration:none;"><em>View All Tips</em></a></span>');
-                        descrpt.push(t);
-                    }
-                    localStorage.setItem('tipInfo'+files, JSON.stringify(descrpt)); //adding items to local storage
-                    },
-                error: function(response){
-                    descrpt.push(["couldn't access file","error"]);
-                }
-            })
-        }
+    
+        var firstTime = localStorage.getItem("first_time");
+        if(!firstTime) {
+            window.localStorage.setItem('tipCount',0);
+            window.localStorage.setItem('priorityCount',0);
             
-        //display tip
-        localStorage.setItem("first_time","1");
-    }
-
+            //acquiring tipInformation
+            for (let files=0; files<fileNames.length; files++){
+                jQuery.ajax({
+                    url: fileNames[files],
+                    success: function(results){
+                        let allInfo=results;
+                        allInfo=allInfo.split("<h2>");
+                        var descrpt=[];
+                        for (let i=0; i<allInfo.length; i++){
+                            var t=[];
+                            t.push(allInfo[i].substring(0, allInfo[i].indexOf("</h2>")));
+                            var link="http://manual.umple.org/manual/"+htmlNames[files];
+                            t.push(allInfo[i].substring(allInfo[i].indexOf("</h2>")+6, allInfo[i].length-1)+'<span style="float: right; padding-right: 10px; display:block;"><a href='+link+' style="color:#4d4d4d; text-align:right; text-decoration:none;" target="helppage"><em>View All Tips</em></a></span>');
+                            descrpt.push(t);
+                        }
+                        window.localStorage.setItem('tipInfo'+files, JSON.stringify(descrpt)); //adding items to local storage
+                        },
+                    error: function(response){
+                        descrpt.push(["couldn't access file",error]);
+                    }
+                })
+            }
+            window.localStorage.setItem("first_time","1");
+        }
+    
+    
     // =========================================
     // Tip Pickers
     // =========================================
 
-
-    var priority=this.localStorage.getItem('priorityCount');
-    var tip = JSON.parse(localStorage.getItem('tipInfo'+priority));
-    var num = parseInt(localStorage.getItem('tipCount'));
-
-    if (tip!=null){
-        document.getElementById('styleTip').innerHTML = "<br/>Tip: "+tip[num+1][0] + ' <span onclick="showExtra()" style=" cursor: pointer; color: blue; text-decoration: underline;">Click for more</span>';
-        document.getElementById('extraInfo').innerHTML = tip[num+1][1];
-        if (num+2>=tip.length-1){
-            if (parseInt(priority)+1>=3) //renew data
-                for (let files=0; files<fileNames.length; files++){
-                    jQuery.ajax({
-                        url: fileNames[files],
-                        async: false,
-                        success: function(results){
-                            let allInfo=results;
-                            allInfo=allInfo.split("<h2>");
-                            var descrpt=[];
-                            for (let i=0; i<allInfo.length; i++){
-                                var t=[];
-                                t.push(allInfo[i].substring(0, allInfo[i].indexOf("</h2>")));
-                                var link="http://manual.umple.org/manual/"+htmlNames[files];
-                                t.push(allInfo[i].substring(allInfo[i].indexOf("</h2>")+6, allInfo[i].length-1)+'<span style="float: right; padding-right: 10px; display:block;"><a href='+link+' style="color:#4d4d4d; text-align:right; text-decoration:none;"><em>View All Tips</em></a></span>');
-                                descrpt.push(t);
+    jQuery(document).ajaxStop(function() {
+        var priority=window.localStorage.getItem('priorityCount');
+        var tip = JSON.parse(window.localStorage.getItem('tipInfo'+priority));
+        var num = parseInt(window.localStorage.getItem('tipCount'));
+        
+        if (tip!=null){
+            document.getElementById('styleTip').innerHTML = "<br/>Tip: "+tip[num+1][0] + ' <span onclick="showExtra()" style=" cursor: pointer; color: blue; text-decoration: underline;">Click for more</span>';
+            document.getElementById('extraInfo').innerHTML = tip[num+1][1];
+            if (num+2>=tip.length-1){
+                if (parseInt(priority)+1>=3) //renew data
+                    for (let files=0; files<fileNames.length; files++){
+                        jQuery.ajax({
+                            url: fileNames[files],
+                            success: function(results){
+                                let allInfo=results;
+                                allInfo=allInfo.split("<h2>");
+                                var descrpt=[];
+                                for (let i=0; i<allInfo.length; i++){
+                                    var t=[];
+                                    t.push(allInfo[i].substring(0, allInfo[i].indexOf("</h2>")));
+                                    var link="http://manual.umple.org/manual/"+htmlNames[files];
+                                    t.push(allInfo[i].substring(allInfo[i].indexOf("</h2>")+6, allInfo[i].length-1)+'<span style="float: right; padding-right: 10px; display:block;"><a href='+link+' style="color:#4d4d4d; text-align:right; text-decoration:none;" target="helppage"><em>View All Tips</em></a></span>');
+                                    descrpt.push(t);
+                                }
+                                localStorage.setItem('tipInfo'+files, JSON.stringify(descrpt)); //adding items to local storage
+                                },
+                            error: function(response){
+                                descrpt.push(["couldn't access file","error"]);
                             }
-                            localStorage.setItem('tipInfo'+files, JSON.stringify(descrpt)); //adding items to local storage
-                            },
-                        error: function(response){
-                            descrpt.push(["couldn't access file","error"]);
-                        }
-                    })
-                    this.localStorage.setItem('priorityCount', '0');
-                }
-                
-            else this.localStorage.setItem('priorityCount', parseInt(priority)+1);
-            localStorage.setItem('tipCount','0');
+                        })
+                        this.localStorage.setItem('priorityCount', '0');
+                    }
+                    
+                else this.localStorage.setItem('priorityCount', parseInt(priority)+1);
+                localStorage.setItem('tipCount','0');
+            }
+            else{
+                localStorage.setItem('tipCount',num+1);
+            }
         }
-        else{
-            localStorage.setItem('tipCount',num+1);
-        }
-    }
-    let currentTime=new Date();
-    currentTime.setTime(currentTime.getTime() + (24*60*60*1000));
-    document.cookie="tipCookie=done; expires="+currentTime.toUTCString()+"; path=/";
+        let currentTime=new Date();
+        currentTime.setTime(currentTime.getTime() + (24*60*60*1000));
+        document.cookie="tipCookie=done; expires="+currentTime.toUTCString()+"; path=/";
+    });
+
+ 
 }
 
 // =========================================
-// Hover Functions
+// Display/Hide Functions
 // =========================================
 
 function showExtra(){
@@ -110,5 +104,3 @@ function hideExtra(){
     document.getElementById('extraInfo').classList.remove("fade-in");
     document.getElementById('extraInfo').classList.add("fade-outInst");
 }
-
-alert("finished with file");
