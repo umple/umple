958356f4f88178ee0fdaceb8a0972148c9f62bf0
Ralph <rngas023@uottawa.ca>
2021-08-06 08:35:44 -0400

    another attempt to make the test run on jenkins

--- umpleonline/testsuite/spec/spec_helper.rb
+++ umpleonline/testsuite/spec/spec_helper.rb
@@ -17,7 +17,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless disable-gpu disable-dev-shm-usage) }
+    chromeOptions: { args: %w(headless disable-gpu no-sandbox disable-dev-shm-usage) }
   )
 
   Capybara::Selenium::Driver.new app,
