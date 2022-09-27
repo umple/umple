7628fe0a6a5f7765f9a7854ed83fa6550c7af0a8
RJStead <43275888+RJStead@users.noreply.github.com>
2022-09-27 17:02:02 -0400

    Initial implementation comparison test for Python
    Added the same tests that are present in other languages. Currently, there are still a lot of failing tests, however we can track progress using the JUnit framework.

---
+++ cruise.umple/test/cruise/umple/implementation/py/PythonManyToMStarTest.java
@@ -0,0 +1,17 @@
+package cruise.umple.implementation.py;
+
+import org.junit.*;
+import cruise.umple.implementation.*;
+
+public class PythonManyToMStarTest extends ManyToMStarTest
+{
+
+  @Before
+  public void setUp()
+  {
+    super.setUp();
+    language = "Python";
+    languagePath = "py";
+  }
+  
+}
