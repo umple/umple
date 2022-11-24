ce68b676f40750df94a8e328c14c7531415cbe0a
RJStead <43275888+RJStead@users.noreply.github.com>
2022-11-24 17:31:36 -0500

    Added Execution tests for Python

---
+++ testbed_python/test/attributes/const_test.py
@@ -0,0 +1,46 @@
+import unittest
+
+# Test classes are imported dynamically
+# Must be generated into ImportModules' namespace then imported
+from ImportModules import importModules
+
+importModules(["ConstDefault", "ConstDefaultInterfaceObject"], ["associations", "test"])
+from ImportModules import *
+import datetime
+
+
+class ConstTest(unittest.TestCase):
+    @unittest.skip("Static variables of ConstDefault are incorrect")
+    def test_constant(self):
+        cd = ConstDefault.ConstDefault()
+        currentDate = datetime.datetime.now()
+
+        self.assertEqual(0, cd.I1)
+        self.assertEqual(0, cd.I2)
+        self.assertEqual(0.0, cd.D1, 0.0)
+        self.assertEqual(0.0, cd.D2, 0.0)
+        self.assertEqual(0.0, cd.F1, 0.0)
+        self.assertEqual(0.0, cd.F2, 0.0)
+        self.assertEqual(False, cd.B1)
+        self.assertEqual(False, cd.B2)
+        self.assertEqual("", cd.STR)
+        self.assertEqual(datetime.strptime("00:00:00", "%H:%M:%S"), cd.TIME)
+        self.assertEqual(str(currentDate), str(cd.DATE))
+
+    @unittest.skip("Static variables of ConstDefault are incorrect")
+    def test_constantInterface(self):
+        cd = ConstDefaultInterfaceObject.ConstDefaultInterfaceObject()
+
+        currentDate = datetime.datetime.now()
+
+        self.assertEqual(0, cd.I1)
+        self.assertEqual(0, cd.I2)
+        self.assertEqual(0.0, cd.D1, 0.0)
+        self.assertEqual(0.0, cd.D2, 0.0)
+        self.assertEqual(0.0, cd.F1, 0.0)
+        self.assertEqual(0.0, cd.F2, 0.0)
+        self.assertEqual(False, cd.B1)
+        self.assertEqual(False, cd.B2)
+        self.assertEqual("", cd.STR)
+        self.assertEqual(datetime.strptime("00:00:00", "%H:%M:%S"), cd.TIME)
+        self.assertEqual(str(currentDate), str(cd.DATE))
