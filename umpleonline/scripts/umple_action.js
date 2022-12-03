6b6e2ecc852516400e7287a012096c288b898cfe
RJStead <43275888+RJStead@users.noreply.github.com>
2022-12-02 19:59:22 -0500

    Removed Python from AV

--- umpleonline/scripts/umple_action.js
+++ umpleonline/scripts/umple_action.js
@@ -1914,16 +1914,6 @@ Action.setCaretPosition = function(line)
       // Special backdoor to turn on experimental features
       document.getElementById('advancedMode').value=1;
 
-      // Add python option
-      if (!!!document.getElementById("genpython")) {
-
-        var pythonOption = document.createElement("option");
-        pythonOption.id = "genpython";
-        pythonOption.value = "python:Python";
-        pythonOption.text = "Python (Alpha Build)";
-        
-        document.getElementById("inputGenerateCode").add(pythonOption,5);
-      }
 
       Page.setFeedbackMessage("");
       return;
