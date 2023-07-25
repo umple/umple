7c5ca2cc62077d48c9faee19143a919da96b4379
Deepank kartikey <deepankkartikey@gmail.com>
2023-07-25 09:58:16 -0400

    Remove CM5 for selectClassInState functionality

--- umpleonline/scripts/umple_action.js
+++ umpleonline/scripts/umple_action.js
@@ -1773,19 +1773,23 @@ Action.stateClicked = function(identifier)
       console.log("stateClicked - if - nested case")
       identifierState=identifierState.split('.');
       selectionIndicies=Action.selectStateInClass(identifierClass,identifierSM,identifierState[0]);
+      console.log("selectionIndicies: ", selectionIndicies)
       console.log("identifierState.length: ", identifierState.length)
       for(let i=1;i<identifierState.length;i++){
         console.log("Inside for-loop... iterating states within a state")
         selectionIndicies=Action.selectStateInState(selectionIndicies.startIndex,selectionIndicies.endIndex,identifierState[i]);
+        // selectionIndiciesCM6=Action.selectStateInStateCM6(selectionIndicies.startIndex,selectionIndicies.endIndex,identifierState[i]);
       }
     } else { //base case
       console.log("stateClicked - else - base case")
-      selectionIndicies=Action.selectStateInClass(identifierClass,identifierSM,identifierState);
-      console.log("selectionIndicies: ", selectionIndicies)
+      // Removing CM5
+      // selectionIndicies=Action.selectStateInClass(identifierClass,identifierSM,identifierState);
+      // console.log("selectionIndicies: ", selectionIndicies)
       selectionIndiciesCM6 = Action.selectStateInClassCM6(identifierClass,identifierSM,identifierState);
       console.log("selectionIndiciesCM6: ", selectionIndiciesCM6)
     }
-    Action.highlightByIndex(selectionIndicies.startIndex,selectionIndicies.endIndex);
+    // Removing CM5
+    // Action.highlightByIndex(selectionIndicies.startIndex,selectionIndicies.endIndex);
     Action.highlightByIndexCM6(selectionIndiciesCM6.startIndex, selectionIndiciesCM6.endIndex);
 
 
@@ -2927,7 +2931,11 @@ Action.selectState = function(stateName)
     var scursor = new RegExp("(class|interface|trait) "+stateName+"($|\\\s|[{])");
     var ncursor = new RegExp("(class|interface|trait) [A-Za-z]");
 
-  Action.selectItem(scursor, ncursor);
+  // Removing CM5
+  // Action.selectItem(scursor, ncursor);
+
+  var selectionIndiciesCM6 = Action.selectItemCM6(scursor, ncursor, className);
+  Action.highlightByIndexCM6(selectionIndiciesCM6.startIndex, selectionIndiciesCM6.endIndex);
 }
 
 Action.splitStates=function(inputStr){
@@ -3100,7 +3108,9 @@ Action.selectStateInClassCM6 = function(className, smName, stateName)
 Action.selectStateInState = function(startIndex,endIndex,target){
   console.log("Debug: Inside selectStateInState")
   let temp=Page.codeMirrorEditor.getValue().substr(startIndex,endIndex-startIndex);
+  console.log("code for NestedState: ", temp)
   let states=Action.splitStates(temp.substr(temp.indexOf("{")+1));
+  console.log("states: ", states)
   var stateFin=null;
   for(let i=0;i<states.length;i++){
     if(states[i].startsWith(target)){
@@ -3108,11 +3118,33 @@ Action.selectStateInState = function(startIndex,endIndex,target){
       break;
     }
   }
+  console.log("stateFin: ", stateFin)
   let outputStart=temp.indexOf(stateFin)+startIndex;
   let outputEnd=outputStart+stateFin.length;
   let outputObj={startIndex:outputStart,endIndex:outputEnd};
   return outputObj;
 }
+
+// Action.selectStateInStateCM6 = function(startIndex,endIndex,target){
+//   console.log("Debug: Inside selectStateInState CM6")
+//   var temp = Page.codeMirrorEditor6.state.doc.toString().substr(startIndex,endIndex-startIndex);
+//   console.log("code for NestedState: ", temp)
+//   let states=Action.splitStates(temp.substr(temp.indexOf("{")+1));
+//   console.log("states: ", states)
+//   var stateFin=null;
+//   for(let i=0;i<states.length;i++){
+//     if(states[i].startsWith(target)){
+//       stateFin=states[i];
+//       break;
+//     }
+//   }
+//   console.log("stateFin: ", stateFin)
+//   let outputStart=temp.indexOf(stateFin)+startIndex;
+//   let outputEnd=outputStart+stateFin.length;
+//   let outputObj={startIndex:outputStart,endIndex:outputEnd};
+//   return outputObj;
+// }
+
 Action.highlightByIndex = function(startIndex,endIndex){
   Page.codeMirrorEditor.setSelection(Action.indexToPos(startIndex,Page.codeMirrorEditor.getValue()),
   Action.indexToPos(endIndex,Page.codeMirrorEditor.getValue()))
