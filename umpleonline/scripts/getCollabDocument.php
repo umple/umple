1ca01776fe089b8a9dc08633d847e6a0476b48b6
Deepank kartikey <deepankkartikey@gmail.com>
2023-08-16 07:28:58 -0400

    Php APIs to trigger collab functions in backend

---
+++ umpleonline/scripts/getCollabDocument.php
@@ -0,0 +1,20 @@
+<?php
+function getDocument($directoryName, $userSessionID){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"{$GLOBALS['EXECUTION_SERVER']}/collab");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "directory={$directoryName}&userID={$userSessionID}&operation={'getDocument'}");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $content = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $error_msg = curl_error($ch);
+    }
+    curl_close($ch);
+
+    if (isset($error_msg)) {
+    return "Issue with Getting Collab Document.\n".$error_msg;
+    } else {
+        return $content;
+    }
+}
+?>
---
+++ umpleonline/scripts/pullCollabUpdates.php
@@ -0,0 +1,20 @@
+<?php
+function pullUpdates($directoryName, $userSessionID){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"{$GLOBALS['EXECUTION_SERVER']}/collab");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "directory={$directoryName}&userID={$userSessionID}&operation={'pullUpdates'}");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $content = curl_exec($ch);
+    if (curl_errno($ch)) {
+        $error_msg = curl_error($ch);
+    }
+    curl_close($ch);
+
+    if (isset($error_msg)) {
+    return "Issue with Pulling Collab Updates.\n".$error_msg;
+    } else {
+        return $content;
+    }
+}
+?>
