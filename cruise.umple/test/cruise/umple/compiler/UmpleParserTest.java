7feb06d7a77073ce4aad58428782391e8bc1e65b
AxelTWC <axel0328@outlook.com>
2023-11-13 17:58:53 -0500

    Add Ignore Test
    Ignoring Test since the Parser needs to compile twice and Jenkins only compile the Compiler once.

--- cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
+++ cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
@@ -522,6 +522,7 @@ public class UmpleParserTest
     assertHasWarningsParse("1012_operationNotFound1.ump", 1012);
     assertHasWarningsParse("1014_operationNotFound1.ump", 1014);
   }
+  @Ignore
   @Test
   public void extraBracketsWarning()
   {
