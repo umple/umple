8c878b742d78575d1a10e869b00d7e6473949788
AxelTWC <axel0328@outlook.com>
2023-11-23 13:04:08 -0500

    Update bumple.bat
    Just changing the time description

--- dev-tools-windows/bumple.bat
+++ dev-tools-windows/bumple.bat
@@ -1,6 +1,6 @@
 cd %~dp0..\build
 echo Doing FULL build of Umple at \build using ant -Dmyenv=wlocal
 echo Building repo at %cd%
-echo This should take 2-5 minutes. Do not interrupt.
+echo This should take 9-12 minutes. Do not interrupt.
 echo You should always have done 'git pull' before running this and resolved conflicts
 ant -Dmyenv=wlocal
