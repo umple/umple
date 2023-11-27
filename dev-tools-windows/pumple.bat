ee75d8e589f1e472e3913027c69a0f50286e2ca9
AxelTWC <axel0328@outlook.com>
2023-11-27 17:49:48 -0500

    Removing Server Stuff as it is different in Windows from Linux and Mac

--- dev-tools-windows/pumple.bat
+++ dev-tools-windows/pumple.bat
@@ -3,5 +3,3 @@ echo Copying jars from dist director to the umpleonline scripts directory
 echo Do this after qfbumple when resting umpleonline
 ant -DshouldPackageUmpleOnline=true -Dmyenv=wlocal -f build.umple.xml packageUmpleonline
 cd %~dp0..\umpleonline\scripts
-call quitserver.bat
-call startexecdocker.bat
--- dev-tools-windows/quitserver.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0..\umpleonline\scripts
-echo Quitting any umpleonline server that is running in $UMPLEROOT/umpleonline/scripts
-php UmpleServerTest.php server -quit 
