2ea682ac9b55c6efcecb9cd575cab2008dcdd79d
Patrik <PatrikLazsloKelemen@gmail.com>
2023-11-27 17:07:54 -0500

    Added 3 modified gaurd tests

--- cruise.umple/test/cruise/umple/statemachine/implementation/py/PythonStateMachineTest.java
+++ cruise.umple/test/cruise/umple/statemachine/implementation/py/PythonStateMachineTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import cruise.umple.statemachine.implementation.StateMachineTest;
 import cruise.umple.util.SampleFileWriter;
+import cruise.umple.compiler.Event;
 
 public class PythonStateMachineTest extends StateMachineTest
 {
@@ -25,5 +26,33 @@ public class PythonStateMachineTest extends StateMachineTest
     assertUmpleTemplateFor(languagePath + "/testTwoParameterGuardPython.ump",languagePath + "/testTwoParameterGuard."+ languagePath +".txt","A_Guard");
   }
 
+  @Override
+  @Test
+  public void guardNameBothAttributeAndMethod()
+  {
+	// Reset autotransition counter so isn't carried over to the next test (it's passed from the java test to the php test)
+	Event.setNextAutoTransitionId(1); 
+	assertUmpleTemplateFor(languagePath + "/guardNameBothAttributeAndMethodPython.ump",languagePath + "/guardNameBothAttributeAndMethod."+ languagePath +".txt","A");
+	Event.setNextAutoTransitionId(1); 
+  }
+  
+  @Override
+  @Test
+  public void guardNameBothAttributeAndMethod2()
+  {
+	Event.setNextAutoTransitionId(1); 
+	assertUmpleTemplateFor(languagePath + "/guardNameBothAttributeAndMethod2Python.ump",languagePath + "/guardNameBothAttributeAndMethod2."+ languagePath +".txt","A");
+    Event.setNextAutoTransitionId(1);
+  }
+  
+
+  @Override
+  @Test
+  public void guardNameBothAttributeAndMethod3()
+  {
+	Event.setNextAutoTransitionId(1); 
+	assertUmpleTemplateFor(languagePath + "/guardNameBothAttributeAndMethod3Python.ump",languagePath + "/guardNameBothAttributeAndMethod3."+ languagePath +".txt","A");
+    Event.setNextAutoTransitionId(1);
+  }
 
 }
