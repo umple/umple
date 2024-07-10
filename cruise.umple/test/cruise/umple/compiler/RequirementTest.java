83f2153da88829340584deebd1b00405b0cf928e
vybhavigururaj <vybhavisg@gmail.com>
2024-07-10 04:48:14 -0400

    Modified the convertToComment method.

--- cruise.umple/test/cruise/umple/compiler/RequirementTest.java
+++ cruise.umple/test/cruise/umple/compiler/RequirementTest.java
@@ -16,11 +16,14 @@ public class RequirementTest
 {
   
   ArrayList<Requirement> allTestRequirements;
+  ArrayList<ReqImplementation> allTestRequirementsImpl;
+  UmpleModel model;
   
   @Before
   public void setUp()
   {
     allTestRequirements = new ArrayList<Requirement>();
+    allTestRequirementsImpl = new ArrayList<ReqImplementation>();
   }
   
   @Test
@@ -33,7 +36,7 @@ public class RequirementTest
   @Test
   public void format_null()
   {
-    String output = Requirement.format("Slashes",allTestRequirements);
+    String output = Requirement.format("Slashes",allTestRequirementsImpl,model);
     Assert.assertEquals(null,output);
   }
   
@@ -41,7 +44,8 @@ public class RequirementTest
   public void format_oneRequirement()
   {
     allTestRequirements.add(new Requirement("R01","a",""));
-    String output = Requirement.format("Slashes",allTestRequirements);
+    //allTestRequirementsImpl.add(new ReqImplementation("R01",reqTok));
+    String output = Requirement.format("Slashes",allTestRequirementsImpl,model);
     Assert.assertEquals("// R01: a",output);
   }
   
@@ -51,7 +55,7 @@ public class RequirementTest
   {
     allTestRequirements.add(new Requirement("R01","a",""));
     allTestRequirements.add(new Requirement("R02","a2",""));
-    String output = Requirement.format("Slashes",allTestRequirements);
+    String output = Requirement.format("Slashes",allTestRequirementsImpl,model);
     Assert.assertEquals("// R01: a\n// R02: a2",output);
   }
 
