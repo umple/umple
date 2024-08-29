3b063ebc757134f57c137ca380ed7c779d6da2a6
vybhavigururaj <vybhavisg@gmail.com>
2024-08-29 00:58:31 -0400

    Updated code to handle all entites with modified tests.

--- cruise.umple/test/cruise/umple/implementation/requirements/RequirementsPlainTest.java
+++ cruise.umple/test/cruise/umple/implementation/requirements/RequirementsPlainTest.java
@@ -33,8 +33,8 @@ public class RequirementsPlainTest extends TemplateTest
     SampleFileWriter.destroy(pathToInput + "/requirements/SortOneReq.html");
     SampleFileWriter.destroy(pathToInput + "/requirements/SortMultipleReqs.html");
     SampleFileWriter.destroy(pathToInput + "/requirements/SortReqsStartA.html");
-    //SampleFileWriter.destroy(pathToInput + "/requirements/ReqHideNotImpl.html");
-    //SampleFileWriter.destroy(pathToInput + "/requirements/ReqNotImpl.html");
+    SampleFileWriter.destroy(pathToInput + "/requirements/ReqHideNotImpl.html");
+    SampleFileWriter.destroy(pathToInput + "/requirements/ReqNotImpl.html");
     SampleFileWriter.destroy(pathToInput + "/requirements/ReqMultipleItems.html");
   }
 
@@ -80,18 +80,17 @@ public class RequirementsPlainTest extends TemplateTest
     assertUmpleTemplateFor("requirements/SortReqsStartA.ump","requirements/SortReqsStartA.html.txt");
   }
   
-  /*@Test
+  @Test
   public void ReqHideNotImpl()
   {
     assertUmpleTemplateFor("requirements/ReqHideNotImpl.ump","requirements/ReqHideNotImpl.html.txt");
-  }*/
+  }
   
-  /*@Test
+  @Test
   public void ReqNotImpl()
   {
     assertUmpleTemplateFor("requirements/ReqNotImpl.ump","requirements/ReqNotImpl.html.txt");
   }
-  */
    @Test
   public void ReqMultipleItems()
   {
