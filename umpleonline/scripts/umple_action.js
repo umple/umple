6bf2711f66fe70f19344b6cc5bff91cccf5192c8
Mohammadreza Davoudi (Payam) <payam.d7@gmail.com>
2024-10-07 17:49:26 -0400

    paasing tests

--- umpleonline/scripts/umple_action.js
+++ umpleonline/scripts/umple_action.js
@@ -4458,56 +4458,56 @@ Action.getCaretPosition = function() // TIM Returns the line number
 
 // we no longer need this for codemirror 6
 // // The following from http://stackoverflow.com/questions/263743/how-to-get-cursor-position-in-textarea/3373056#3373056
-// Action.getInputSelectionStart = function(el) 
-// {
-//   var start = 0, normalizedValue, range, textInputRange, len, endRange;
+Action.getInputSelectionStart = function(el) 
+{
+  var start = 0, normalizedValue, range, textInputRange, len, endRange;
 
-//   if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") 
-//   {
-//     start = el.selectionStart;
-//     // The following for debugging
-//     if (Page.getAdvancedMode() == 2) 
-//     { // debug
-//       Page.setFeedbackMessage("Non-IE browser ");
-//     }
-//   }
-//   else 
-//   { // IE Support
-//     // The following for debugging
-//     if (Page.getAdvancedMode() == 2) // debug
-//     { 
-//       Page.setFeedbackMessage("IE-type browser ");
-//     }
-//     range = document.selection.createRange();
+  if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") 
+  {
+    start = el.selectionStart;
+    // The following for debugging
+    if (Page.getAdvancedMode() == 2) 
+    { // debug
+      Page.setFeedbackMessage("Non-IE browser ");
+    }
+  }
+  else 
+  { // IE Support
+    // The following for debugging
+    if (Page.getAdvancedMode() == 2) // debug
+    { 
+      Page.setFeedbackMessage("IE-type browser ");
+    }
+    range = document.selection.createRange();
     
-//     if (range && range.parentElement() == el) {
-//       len = el.value.length;
-//       normalizedValue = el.value.replace(/\r\n/g, "\n");
+    if (range && range.parentElement() == el) {
+      len = el.value.length;
+      normalizedValue = el.value.replace(/\r\n/g, "\n");
     
-//       // Create a working TextRange that lives only in the input
-//       textInputRange = el.createTextRange();
-//       textInputRange.moveToBookmark(range.getBookmark());
+      // Create a working TextRange that lives only in the input
+      textInputRange = el.createTextRange();
+      textInputRange.moveToBookmark(range.getBookmark());
     
-//       // Check if the start and end of the selection are at the very end
-//       // of the input, since moveStart/moveEnd doesn't return what we want
-//       // in those cases
-//       endRange = el.createTextRange();
-//       endRange.collapse(false);
+      // Check if the start and end of the selection are at the very end
+      // of the input, since moveStart/moveEnd doesn't return what we want
+      // in those cases
+      endRange = el.createTextRange();
+      endRange.collapse(false);
     
-//       if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) 
-//       {
-//         start = len;
-//       } 
-//       else 
-//       {
-//         start = -textInputRange.moveStart("character", -len);
-//         start += normalizedValue.slice(0, start).split("\n").length - 1;
-//       }
-//     }
-//   }
+      if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) 
+      {
+        start = len;
+      } 
+      else 
+      {
+        start = -textInputRange.moveStart("character", -len);
+        start += normalizedValue.slice(0, start).split("\n").length - 1;
+      }
+    }
+  }
 
-//   return start;
-// }
+  return start;
+}
 
 
 Action.setCaretPosition = function(line)
