a384dd5525d1b88c483fc593f89c687fa8ec1173
Mohammadreza Davoudi (Payam) <payam.d7@gmail.com>
2024-12-02 16:34:33 -0500

    Clean up umple_page

--- UmpleCollabServer/src/server.ts
+++ UmpleCollabServer/src/server.ts
@@ -1,3 +1,7 @@
+// Copyright: All contributers to the Umple Project
+// This file is made available subject to the open source license found at:
+// http://umple.org/license
+
 // This provides collaboration server functionality to allow multiple
 // collaborations among Codemirror6 clients. 
 //
--- umpleonline/scripts/umpleCollab.js
+++ umpleonline/scripts/umpleCollab.js
@@ -1,3 +1,7 @@
+// Copyright: All contributers to the Umple Project
+// This file is made available subject to the open source license found at:
+// http://umple.org/license
+
 // console.log("umpleCollab.js loaded ...");
 
 Collab = new Object();
