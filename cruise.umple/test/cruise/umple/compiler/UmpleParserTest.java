a92740af62d9c9fa2d46d4bdd041a76276ba989d
Ilyas Ait Ali <ilyasgameraa@gmail.com>
2026-03-06 11:09:48 -0500

    Address PR review comments: add warning 121 validation, minimal test, and explanatory comment

--- cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
+++ cruise.umple/test/cruise/umple/compiler/UmpleParserTest.java
@@ -1864,7 +1864,12 @@ public class UmpleParserTest
 
   @Test
   public void TestInvalidAssociationRoleNameError120() {
-          assertFailedParse("2341_invalidAssociationRoleName.ump",120);
+    assertFailedParse("2341_invalidAssociationRoleName.ump",120);
+  }
+
+  @Test
+  public void TestAssociationRoleNameWarning121() {
+      assertHasWarningsParse("2341_roleNameUppercaseWarning.ump",121);
   }
 
   @Test
