d993c9cbfd6e27db24c86f38cc56413a8f585abb
sky jiang <44115200+skyfackr@users.noreply.github.com>
2026-03-26 16:20:16 +0000

    fix ajax deadlock

--- umpleonline/scripts/ajax.js
+++ umpleonline/scripts/ajax.js
@@ -81,10 +81,18 @@ function AjaxSender(aUrl,aCallback,aPostData)
   
     http.onreadystatechange = 
       function () {
-        if (http.readyState != 4) { return; }
-        if (http.status != 200 && http.status != 304) { return; }
-        callback(http);
-        Ajax.isSending = false;
+        try{
+          // The ready state will always be 4 after request ends. Whatever error or not.
+          if (http.readyState != 4) { return; }
+          if (http.status != 200 && http.status != 304) { return; }
+          // There is no error callback? Looks like the code here don't want to run callback if the status is not success.
+          // Keep this behavior here currently
+          callback(http);
+        }
+        finally{
+
+          Ajax.isSending = false;
+        }
       }
   
     if (http.readyState == 4) return;
