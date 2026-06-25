89f2a570e5061cbd19e2399258fc28f4b11ff235
DeveshKhanna7 <deveshkhanna7@gmail.com>
2026-06-25 19:38:49 +0530

    Restrict generateDefault to allowed values to prevent XSS

--- umpleonline/umple.php
+++ umpleonline/umple.php
@@ -129,9 +129,27 @@ if (isset($_REQUEST["model"]) && explode("-", $_REQUEST["model"])[0] == "task")
 }
 
 //
-$generateDefault="#genclass";
-if (isset($_REQUEST['generateDefault']) && $_REQUEST["generateDefault"] != "") {
-  $generateDefault="#gen".strip_tags($_REQUEST['generateDefault']);
+$generateDefault = "#genclass";
+
+$allowedGenerateDefaults = array(
+    "java",
+    "javadoc",
+    "php",
+    "cpp",
+    "ruby",
+    "sql",
+    "metrics",
+    "alloy",
+    "nusmv",
+    "uigu2"
+);
+
+if (isset($_REQUEST['generateDefault']) && $_REQUEST["generateDefault"] !== "") {
+    $requested = trim($_REQUEST["generateDefault"]);
+
+    if (in_array($requested, $allowedGenerateDefaults, true)) {
+        $generateDefault = "#gen" . $requested;
+    }
 }
 
 $output = $dataHandle->readData('model.ump');
