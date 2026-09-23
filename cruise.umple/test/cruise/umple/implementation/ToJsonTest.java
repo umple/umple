415970bb206c6b48b63f7a4a0b345a0d145aac99
Jatin Kumar <jatingartan109@gmail.com>
2026-09-23 13:59:21 -0400

    fix(genJson): save and restore state machine states, Fixes #2504

--- cruise.umple/test/cruise/umple/implementation/ToJsonTest.java
+++ cruise.umple/test/cruise/umple/implementation/ToJsonTest.java
@@ -25,4 +25,11 @@ public class ToJsonTest extends TemplateTest
   {
     assertUmpleTemplateFor("ToJsonTest_1.ump",languagePath + "/ToJsonTest_1_Address."+ languagePath +".txt","Address");
   } 
+
+  // Issue 2504
+  @Test
+  public void GenJsonTest_3StateMachines()
+  {
+    assertUmpleTemplateFor("ToJsonTest_3.ump",languagePath + "/ToJsonTest_3_Player."+ languagePath +".txt","Player");
+  }
 }
